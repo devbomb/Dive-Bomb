165505bd60cca6c548f3f5492f325e03c0aa2314
Alex Shelley <ashelley@purdue.edu>
2023-08-27 16:45:21 -0400

    Change the camera to be a simple orbit cam

--- Scripts/Entities/Player/FollowCamera.cs
+++ Scripts/Entities/Player/FollowCamera.cs
@@ -6,28 +6,37 @@ namespace FastDragon
     {
         [Export] public Node3D FollowTarget;
         [Export] public float FollowDistance = 6;
-        [Export] public float FollowHeight = 3;
+        [Export] public float MouseSensitivity = 0.0001f;
+        [Export] public float MinOrbitPitchDeg = -90;
+        [Export] public float MaxOrbitPitchDeg = 0;
+
+        private float _orbitYawRad;
+        private float _orbitPitchRad;
+
+        public override void _Input(InputEvent ev)
+        {
+            if (ev is InputEventMouseMotion mouse)
+            {
+                _orbitYawRad += mouse.Velocity.X * MouseSensitivity;
+                _orbitPitchRad += mouse.Velocity.Y * MouseSensitivity;
+
+                _orbitYawRad = Mathf.PosMod(_orbitYawRad, Mathf.DegToRad(360));
+                _orbitPitchRad = Mathf.Clamp(
+                    _orbitPitchRad,
+                    Mathf.DegToRad(MinOrbitPitchDeg),
+                    Mathf.DegToRad(MaxOrbitPitchDeg)
+                );
+            }
+        }
 
         public override void _Process(double deltaD)
         {
-            // Move the camera toward the target on the x/z plane
-            var flatTargetPos = FollowTarget.GlobalPosition.Flattened();
-            var newPos = GlobalPosition.Flattened();
-            float distance = newPos.DistanceTo(flatTargetPos);
+            Vector3 dir = Vector3.Back
+                .Rotated(Vector3.Right, _orbitPitchRad)
+                .Rotated(Vector3.Up, _orbitYawRad);
 
-            if (distance > FollowDistance)
-            {
-                distance -= FollowDistance;
-                newPos += newPos.DirectionTo(flatTargetPos) * distance;
-            }
+            GlobalPosition = FollowTarget.GlobalPosition + (dir * FollowDistance);
 
-            // Always keep it locked at the desired height
-            newPos.Y = FollowTarget.GlobalPosition.Y + FollowHeight;
-
-            // Apply the movement
-            GlobalPosition = newPos;
-
-            // Look at the target
             LookAt(FollowTarget.GlobalPosition);
         }
     }
