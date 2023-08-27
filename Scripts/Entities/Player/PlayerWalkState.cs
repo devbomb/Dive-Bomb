a4c0452f5ec84661f6081881b35915c3dbe9e164
Alex Shelley <ashelley@purdue.edu>
2023-08-27 17:08:01 -0400

    Add an actual walk speed

--- Scripts/Entities/Player/PlayerWalkState.cs
+++ Scripts/Entities/Player/PlayerWalkState.cs
@@ -5,17 +5,19 @@ namespace FastDragon
 {
     public partial class PlayerWalkState : PlayerState
     {
+        [Export] public float WalkSpeed = 2.5f;
+
         public override void _PhysicsProcess(double deltaD)
         {
             var leftStick2D = InputService.LeftStick;
             Vector3 leftStick3D =
                 (Vector3.Right * leftStick2D.X) +
                 (Vector3.Forward * leftStick2D.Y);
-            
+
             Vector3 cameraRot = GetViewport().GetCamera3D().Rotation;
             leftStick3D = leftStick3D.Rotated(Vector3.Up, cameraRot.Y);
 
-            _player.Velocity = leftStick3D;
+            _player.Velocity = leftStick3D * WalkSpeed;
             _player.MoveAndSlide();
         }
     }
