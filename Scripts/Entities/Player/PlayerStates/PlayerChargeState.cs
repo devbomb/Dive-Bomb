6b25b8fe90b657d0fed16d0920e200e1d10949c8
Alex Shelley <alexthepeppershelley@gmail.com>
2023-09-01 15:40:53 -0400

    The camera now moves more smoothly in the charging state

--- Scripts/Entities/Player/PlayerStates/PlayerChargeState.cs
+++ Scripts/Entities/Player/PlayerStates/PlayerChargeState.cs
@@ -8,6 +8,8 @@ namespace FastDragon
         [Export] public float ChargeSpeed = 5f;
         [Export] public float TurnSpeedDeg = 90;
 
+        [Export] public float CameraZoomSpeed = 4;
+        [Export] public float CameraRotSpeedDeg = 180;
         [Export] public float CameraPitchDeg = 0;
         [Export] public float CameraDistance = 5;
 
@@ -27,12 +29,33 @@ namespace FastDragon
             _player.Velocity = _player.GlobalForward() * ChargeSpeed;
             _player.MoveAndSlide();
 
-            _player.Camera.OrbitDistance = CameraDistance;
-            _player.Camera.OrbitPitchRad = Mathf.DegToRad(CameraPitchDeg);
-            _player.Camera.OrbitYawRad = _player.GlobalRotation.Y;
+            UpdateCamera(delta);
 
             if (!InputService.ChargeHeld)
                 _player.ChangeState<PlayerWalkState>();
+        }
+
+        private void UpdateCamera(float delta)
+        {
+            var camera = _player.Camera;
+
+            camera.OrbitDistance = Mathf.MoveToward(
+                camera.OrbitDistance,
+                CameraDistance,
+                CameraZoomSpeed * delta
+            );
+
+            camera.OrbitPitchRad = AngleMath.MoveToward(
+                camera.OrbitPitchRad,
+                Mathf.DegToRad(CameraPitchDeg),
+                Mathf.DegToRad(CameraRotSpeedDeg) * delta
+            );
+
+            camera.OrbitYawRad = AngleMath.MoveToward(
+                camera.OrbitYawRad,
+                _player.GlobalRotation.Y,
+                Mathf.DegToRad(CameraRotSpeedDeg) * delta
+            );
         }
     }
 }
