2c679cd7db66a9aba4e4e6667ebfe4030903a0e0
Alex Shelley <alexthepeppershelley@gmail.com>
2023-09-15 20:14:59 -0400

    Add a glide animation

--- Scripts/Entities/Player/PlayerStates/PlayerGlideState.cs
+++ Scripts/Entities/Player/PlayerStates/PlayerGlideState.cs
@@ -8,6 +8,7 @@ namespace FastDragon
         public override void OnStateEntered()
         {
             _player.Camera.ChangeState<OrbitCameraLockedState>();
+            _player.Animator.Play("Glide");
             _player.VSpeed = 0;
         }
 
