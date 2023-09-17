0d265a84ab41b7ceb07892cdfc84db95c77c878a
Alex Shelley <alexthepeppershelley@gmail.com>
2023-09-17 14:58:01 -0400

    Touching a gem now collects it

--- Scripts/Objects/Gem.cs
+++ Scripts/Objects/Gem.cs
@@ -5,5 +5,23 @@ namespace FastDragon
     public partial class Gem : Node3D
     {
         [Export] public GemColor Value;
+        public bool Collected {get; private set;} = false;
+
+        public override void _Process(double delta)
+        {
+            Visible = !Collected;
+        }
+
+        public void OnCollectionAreaBodyEntered(Node3D body)
+        {
+            if (body is Player && !Collected)
+            {
+                SaveFile.Current.TotalGemCount += (int)Value;
+                SaveFile.Current.CollectedGems.Add(GetPath());
+                Collected = true;
+
+                GD.Print($"Collected gem {GetPath()}");
+            }
+        }
     }
 }
