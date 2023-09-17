04553ab170b2461e27c8d72bbc094278e89e1008
Alex Shelley <ashelley@purdue.edu>
2023-09-17 17:15:34 -0400

    Gems now have a "hidden" state

--- Scripts/Objects/Gem.cs
+++ Scripts/Objects/Gem.cs
@@ -5,33 +5,46 @@ namespace FastDragon
     public partial class Gem : Node3D
     {
         [Export] public GemColor Value;
-        public bool Collected {get; private set;} = false;
+
+        public enum State
+        {
+            Hidden,
+            Revealed,
+            Collected
+        }
+        public State CurrentState = State.Revealed;
 
         private Vector3 _initialPos;
+        private State _initialState;
 
         public override void _Ready()
         {
             _initialPos = Position;
+            _initialState = CurrentState;
 
-            SignalBus.Instance.LevelReset += () =>
-            {
-                Position = _initialPos;
-                Collected = SaveFile.Current.CollectedGems.Contains(GetPath());
-            };
+            SignalBus.Instance.LevelReset += Reset;
+        }
+
+        public void Reset()
+        {
+            Position = _initialPos;
+            CurrentState = SaveFile.Current.CollectedGems.Contains(GetPath())
+                ? State.Collected
+                : _initialState;
         }
 
         public override void _Process(double delta)
         {
-            Visible = !Collected;
+            Visible = CurrentState == State.Revealed;
         }
 
         public void OnCollectionAreaBodyEntered(Node3D body)
         {
-            if (body is Player && !Collected)
+            if (body is Player && CurrentState == State.Revealed)
             {
                 SaveFile.Current.TotalGemCount += (int)Value;
                 SaveFile.Current.CollectedGems.Add(GetPath());
-                Collected = true;
+                CurrentState = State.Collected;
 
                 GD.Print($"Collected gem {GetPath()}");
             }
