1b079b90c54222c673578e6725487ce358d92e33
Alex Shelley <alexthepeppershelley@gmail.com>
2023-09-24 13:46:40 -0400

    You can now customize the particle speed

--- Scripts/Objects/Flame/FlameTendril.cs
+++ Scripts/Objects/Flame/FlameTendril.cs
@@ -17,8 +17,15 @@ namespace FastDragon
             set => SetProperty(ref _radius, value);
         }
 
+        [Export] public float ParticleSpeed
+        {
+            get => _particleSpeed;
+            set => SetProperty(ref _particleSpeed, value);
+        }
+
         private float _length = 1.5f;
         private float _radius = 0.25f;
+        private float _particleSpeed = 1;
 
         private Node3D _sphere => GetNode<Node3D>("%Sphere");
         private GpuParticles3D _particles => GetNode<GpuParticles3D>("%FlameParticles");
@@ -27,7 +34,11 @@ namespace FastDragon
         {
             _sphere.Scale = Vector3.One * _radius;
             _sphere.Position = Vector3.Forward * _length;
-            _particles.Lifetime = _length;
+            _particles.Lifetime = _length / _particleSpeed;
+
+            var procMat = (ParticleProcessMaterial)_particles.ProcessMaterial;
+            procMat.InitialVelocityMin = _particleSpeed;
+            procMat.InitialVelocityMax = _particleSpeed;
         }
 
         private void SetProperty(ref float storage, float value)
