3d63428f1f541d921df1b1bf5cca8736f536edfa
Alex Shelley <alexthepeppershelley@gmail.com>
2023-09-30 14:06:26 -0400

    The tendrils are now created programmatically, and can be previewed in the editor

--- Scripts/Objects/Flame.cs
+++ Scripts/Objects/Flame.cs
@@ -5,20 +5,55 @@ using Godot;
 
 namespace FastDragon
 {
+    [Tool]
     public partial class Flame : Node3D
     {
         public const float ActiveDuration = 0.5f;
         public const float CooldownDuration = 7f / 30;
-        public const float Length = 3f;
-        public const float HAngleDeg = 65f;
-        public const float VAngleDeg = 15f;
 
         [Export] public PackedScene FlameTendrilPrefab;
         [Export] public Node3D BodyToIgnore;
 
+        [Export] public float Length
+        {
+            get => _length;
+            set => SetProperty(ref _length, value);
+        }
+        private float _length = 3f;
+
+        [Export] public float ConeAngleDeg
+        {
+            get => _coneAngleDeg;
+            set => SetProperty(ref _coneAngleDeg, value);
+        }
+        private float _coneAngleDeg = 50;
+
+        [Export] public float ConeYScale
+        {
+            get => _coneYScale;
+            set => SetProperty(ref _coneYScale, value);
+        }
+        private float _coneYScale = 0.75f;
+
+        [Export] public int ShellTendrilCount
+        {
+            get => _shellTendrilCount;
+            set => SetProperty(ref _shellTendrilCount, value);
+        }
+        private int _shellTendrilCount = 5;
+
+        [Export] public int InnerRingTendrilCount
+        {
+            get => _innerRingTendrilCount;
+            set => SetProperty(ref _innerRingTendrilCount, value);
+        }
+        private int _innerRingTendrilCount = 4;
+
         public bool AllowFlaming = true;
 
         private Node3D _tendrils => GetNode<Node3D>("%Tendrils");
+
+        private bool _initialized = false;
 
         private enum State
         {
@@ -33,11 +68,12 @@ namespace FastDragon
 
         public override void _Ready()
         {
-            SignalBus.Instance.LevelReset += Reset;
+            _initialized = true;
+            CreateFlameTendrils();
 
-            foreach (var tendril in AllFlameTendrils())
+            if (!Engine.IsEditorHint())
             {
-                tendril.BodyToIgnore = BodyToIgnore;
+                SignalBus.Instance.LevelReset += Reset;
             }
         }
 
@@ -55,6 +91,9 @@ namespace FastDragon
 
         public override void _PhysicsProcess(double deltaD)
         {
+            if (Engine.IsEditorHint())
+                return;
+
             float delta = (float)deltaD;
 
             switch (_currentState)
@@ -115,9 +154,60 @@ namespace FastDragon
             _currentState = State.Ready;
         }
 
+        private void CreateFlameTendrils()
+        {
+            // Clear out existing tendrils
+            foreach (var tendril in AllFlameTendrils().ToArray())
+            {
+                tendril.GetParent().RemoveChild(tendril);
+                tendril.QueueFree();
+            }
+
+            CreateTendril();
+            CreateRing(ShellTendrilCount, ConeAngleDeg);
+            CreateRing(InnerRingTendrilCount, ConeAngleDeg / 2);
+
+            void CreateRing(int tendrilCount, float hAngleDeg)
+            {
+                for (int i = 0; i < tendrilCount; i++)
+                {
+                    float t = ((float)i) / (tendrilCount - 1);
+                    float thetaDeg = Mathf.Lerp(0, 180, t);
+
+                    var tendril = CreateTendril();
+                    Vector3 forward = Vector3.Forward
+                        .Rotated(Vector3.Up, Mathf.DegToRad(hAngleDeg / 2))
+                        .Rotated(Vector3.Forward, Mathf.DegToRad(thetaDeg));
+
+                    forward.Y *= ConeYScale;
+                    forward = forward.Normalized();
+
+                    tendril.Rotation = forward.ForwardToEulerAnglesRad();
+                }
+            }
+
+            FlameTendril CreateTendril()
+            {
+                var tendril = FlameTendrilPrefab.Instantiate<FlameTendril>();
+                tendril.BodyToIgnore = BodyToIgnore;
+                tendril.Length = Length;
+
+                _tendrils.AddChild(tendril);
+                return tendril;
+            }
+        }
+
         private IEnumerable<FlameTendril> AllFlameTendrils()
         {
             return _tendrils.EnumerateChildren().Cast<FlameTendril>();
+        }
+
+        private void SetProperty<T>(ref T storage, T value)
+        {
+            storage = value;
+
+            if (Engine.IsEditorHint() && _initialized && Owner != this)
+                CreateFlameTendrils();
         }
     }
 }
