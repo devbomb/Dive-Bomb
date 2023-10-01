736e954c2fa2df61aaeaa0898addc602f619f845
Alex Shelley <alexthepeppershelley@gmail.com>
2023-10-01 12:55:09 -0400

    Move AllowFlaming to Player instead of Flame.

--- Scripts/Entities/Player/Player.cs
+++ Scripts/Entities/Player/Player.cs
@@ -16,9 +16,10 @@ namespace FastDragon
         /// </summary>
         public bool PretendColliderDisabled {get; set;}
 
+        public bool AllowFlaming => _currentState.AllowFlaming;
+
         public OrbitCamera Camera => GetNode<OrbitCamera>("%Camera");
         public Node3D Model => GetNode<Node3D>("%Model");
-        public Flame Flame => GetNode<Flame>("%Flame");
         public AnimationPlayer Animator => GetNode<AnimationPlayer>("%Animator");
 
         public float FSpeed
@@ -89,7 +90,6 @@ namespace FastDragon
 
             _currentState.ProcessMode = ProcessModeEnum.Inherit;
             _currentState.OnStateEntered();
-            Flame.AllowFlaming = _currentState.AllowFlaming;
         }
 
         private IEnumerable<PlayerState> States()
