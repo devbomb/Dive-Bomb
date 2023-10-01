9b835bbdfcf87798f9e2836d1b51d0373662e565
Alex Shelley <ashelley@purdue.edu>
2023-10-01 18:42:44 -0400

    DeathBarrier now connects to its signals via code, to prevent a weird interaction with TBLoaderWrapper

--- Scripts/Entities/DeathBarrier.cs
+++ Scripts/Entities/DeathBarrier.cs
@@ -4,6 +4,11 @@ namespace FastDragon
 {
     public partial class DeathBarrier : Area3D
     {
+        public override void _Ready()
+        {
+            BodyEntered += OnBodyEntered;
+        }
+
         public void OnBodyEntered(Node3D body)
         {
             if (body is Player p)
