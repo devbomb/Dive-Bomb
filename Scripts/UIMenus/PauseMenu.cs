5c7eab3517b50cb9c595884d5f4d26586cec8e06
Alex Shelley <alexthepeppershelley@gmail.com>
2023-10-05 16:46:37 -0400

    Add a pause menu

---
+++ Scripts/UIMenus/PauseMenu.cs
@@ -0,0 +1,39 @@
+using Godot;
+
+namespace FastDragon
+{
+    public partial class PauseMenu : Control
+    {
+        private bool _open = false;
+
+        public override void _Input(InputEvent ev)
+        {
+            if (InputService.PauseJustPressed(ev))
+            {
+                if (_open)
+                    Close();
+                else
+                    Open();
+            }
+        }
+
+        public override void _Ready()
+        {
+            Close();
+        }
+
+        public void Open()
+        {
+            _open = true;
+            Visible = true;
+            GetTree().Paused = true;
+        }
+
+        public void Close()
+        {
+            _open = false;
+            Visible = false;
+            GetTree().Paused = false;
+        }
+    }
+}
