2c55b4b0d0498be791b07317fa8e5fb19f5adf15
Alex Shelley <alexthepeppershelley@gmail.com>
2023-10-21 15:39:51 -0400

    Add TweenAngleRad() extension

---
+++ Scripts/Utils/TweenExtensions.cs
@@ -0,0 +1,30 @@
+using Godot;
+
+namespace FastDragon
+{
+    public static class TweenExtensions
+    {
+        public static MethodTweener TweenAngleRad(
+            this Tween tween,
+            Node node,
+            StringName property,
+            float toRad,
+            double duration
+        )
+        {
+            float fromRad = (float)node.Get(property);
+            System.Action<float> setter = (float t) =>
+            {
+                float angle = Mathf.LerpAngle(fromRad, toRad, t);
+                node.Set(property, angle);
+            };
+
+            return tween.TweenMethod(
+                Callable.From(setter),
+                0f,
+                1f,
+                duration
+            );
+        }
+    }
+}
