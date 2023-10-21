2cf0e985f26d78612b1220f3617703d1db18ce54
Alex Shelley <alexthepeppershelley@gmail.com>
2023-10-21 15:40:11 -0400

    Rewrite PortalLoadingScreen to use tweens instead of a state machine

--- Scripts/PortalLoadingScreen.cs
+++ Scripts/PortalLoadingScreen.cs
@@ -12,10 +12,6 @@ namespace FastDragon
         private const float MinLoadingWaitTime = 2;
 
         private string _levelSceneFile;
-        private Vector3 _playerStartRotRad;
-        private float _camStartDist;
-        private float _camStartYawRad;
-        private float _camStartPitchRad;
 
         private Node3D _playerModel => GetNode<Node3D>("%PlayerModel");
         private AnimationPlayer _playerAnimator => GetNode<AnimationPlayer>("%PlayerAnimator");
@@ -23,18 +19,13 @@ namespace FastDragon
 
         private WorldEnvironment _worldEnv => GetNode<WorldEnvironment>("%WorldEnv");
 
-        private StateMachine _stateMachine = new StateMachine(typeof(LoadingScreenState));
-
-        public override void _Ready()
-        {
-            AddChild(_stateMachine);
-        }
+        private bool _animationDone;
 
         public void Initialize(
             string levelSceneFile,
             Environment skyBoxEnvironment,
             double animationStartTime,
-            Vector3 playerRotRad,
+            Vector3 playerStartRotRad,
             float cameraDist,
             float cameraYawRad,
             float cameraPitchRad
@@ -42,10 +33,6 @@ namespace FastDragon
         {
             _levelSceneFile = levelSceneFile;
             _worldEnv.Environment = skyBoxEnvironment;
-            _playerStartRotRad = playerRotRad;
-            _camStartDist = cameraDist;
-            _camStartYawRad = cameraYawRad;
-            _camStartPitchRad = cameraPitchRad;
 
             // Start loading the level in the background
             ResourceLoader.LoadThreadedRequest(_levelSceneFile);
@@ -57,11 +44,25 @@ namespace FastDragon
             _camera.ChangeState<OrbitCameraLockedState>();
 
             // Start the loading screen animation
-            _playerModel.GlobalRotation = _playerStartRotRad;
+            _playerModel.GlobalRotation = playerStartRotRad;
             _camera.OrbitDistance = cameraDist;
             _camera.OrbitYawRad = cameraYawRad;
             _camera.OrbitPitchRad = cameraPitchRad;
-            _stateMachine.ChangeState<MovingToRest>();
+            _animationDone = false;
+
+            var tween = CreateTween();
+            tween.TweenProperty(_playerModel, "global_rotation", Vector3.Zero, RestMoveDuration);
+            tween.Parallel().TweenProperty(_camera, "OrbitDistance", CameraDist, RestMoveDuration);
+            tween.Parallel().TweenAngleRadSinusoidal(_camera, "OrbitYawRad", CameraYawRad, RestMoveDuration);
+            tween.Parallel().TweenAngleRadSinusoidal(_camera, "OrbitPitchRad", CameraPitchRad, RestMoveDuration);
+            tween.TweenInterval(MinLoadingWaitTime);
+            tween.TweenCallback(Callable.From(() => _animationDone = true));
+        }
+
+        public override void _Process(double delta)
+        {
+            if (DoneLoading() && _animationDone)
+                GoToTargetMap();
         }
 
         private bool DoneLoading()
@@ -74,75 +75,6 @@ namespace FastDragon
         {
             var mapPrefab = (PackedScene)ResourceLoader.LoadThreadedGet(_levelSceneFile);
             GetTree().ChangeSceneToPacked(mapPrefab);
-        }
-
-        private abstract partial class LoadingScreenState : State
-        {
-            protected PortalLoadingScreen _loadingScreen => _stateMachine.GetParent<PortalLoadingScreen>();
-            protected OrbitCamera _camera => _loadingScreen._camera;
-            protected Node3D _playerModel => _loadingScreen._playerModel;
-        }
-
-        private partial class MovingToRest : LoadingScreenState
-        {
-            private float _timer = 0;
-
-            public override void _PhysicsProcess(double deltaD)
-            {
-                float delta = (float)deltaD;
-
-                _timer += delta;
-
-                float t = _timer / RestMoveDuration;
-
-                _playerModel.GlobalRotation = _loadingScreen
-                    ._playerStartRotRad
-                    .LerpEulerRad(Vector3.Zero, t);
-
-                _camera.OrbitDistance = MathUtils.LerpSinusoidal(
-                    _loadingScreen._camStartDist,
-                    CameraDist,
-                    t
-                );
-
-                _camera.OrbitYawRad = MathUtils.LerpAngleSinusoidal(
-                    _loadingScreen._camStartYawRad,
-                    CameraYawRad,
-                    t
-                );
-
-                _camera.OrbitPitchRad = MathUtils.LerpAngleSinusoidal(
-                    _loadingScreen._camStartPitchRad,
-                    CameraPitchRad,
-                    t
-                );
-
-                if (_timer > RestMoveDuration)
-                    ChangeState<WaitingForLoad>();
-            }
-        }
-
-        private partial class WaitingForLoad : LoadingScreenState
-        {
-            private float _timer = 0;
-
-            public override void OnStateEntered()
-            {
-                _playerModel.GlobalRotation = Vector3.Zero;
-                _camera.OrbitDistance = CameraDist;
-                _camera.OrbitYawRad = CameraYawRad;
-                _camera.OrbitPitchRad = CameraPitchRad;
-
-                _timer = MinLoadingWaitTime;
-            }
-
-            public override void _Process(double delta)
-            {
-                _timer -= (float)delta;
-
-                if (_loadingScreen.DoneLoading() && _timer <= 0)
-                    _loadingScreen.GoToTargetMap();
-            }
         }
     }
 }
