65983897c16307ef8acbb53eca5644024c73eefd
Alex Shelley <alexthepeppershelley@gmail.com>
2023-11-12 21:17:17 -0500

    Don't allocate every frame

--- Scripts/UIWidgets/SpinningCounter3D.cs
+++ Scripts/UIWidgets/SpinningCounter3D.cs
@@ -11,28 +11,31 @@ namespace FastDragon
         [Export] public float DigitSeparation = 0.125f;
         [Export] public int Value;
 
+        private List<int> _digits = new List<int>();
+
         public override void _Process(double delta)
         {
-            int[] digits = EnumerateDigitsIn(Value).ToArray();
+            UpdateDigitsList();
 
             int numChildren = GetChildCount();
             for (int i = 0; i < numChildren; i++)
             {
                 var child = GetChild<SingleDigitCounter3D>(numChildren - i - 1);
 
-                child.Digit = i < digits.Length
-                    ? digits[i]
+                child.Digit = i < _digits.Count
+                    ? _digits[i]
                     : 0;
             }
         }
 
-        private IEnumerable<int> EnumerateDigitsIn(int value)
+        private void UpdateDigitsList()
         {
-            value = Math.Abs(value);
+            _digits.Clear();
+            int value = Math.Abs(Value);
 
             do
             {
-                yield return value % 10;
+                _digits.Add(value % 10);
                 value /= 10;
             }
             while (value > 0);
