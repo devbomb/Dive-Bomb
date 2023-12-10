940b7f480e34964d95ff2396883a047e37294027
Alex Shelley <ashelley@purdue.edu>
2023-12-10 12:46:57 -0500

    Add a more complicated gem counting animation

--- Scripts/PortalLoadingScreen.cs
+++ Scripts/PortalLoadingScreen.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using System.Linq;
 using Godot;
 
@@ -13,6 +15,12 @@ namespace FastDragon
 
         public const float CameraDist = Player.Glide.CameraDistance;
 
+        [Export] public PackedScene RedGemPrefab;
+        [Export] public PackedScene GreenGemPrefab;
+        [Export] public PackedScene PurpleGemPrefab;
+        [Export] public PackedScene YellowGemPrefab;
+        [Export] public PackedScene MagentaGemPrefab;
+
         private float _cameraYawRad => _isReturningHome
             ? ReturnHomeCameraYawRad
             : EnterLevelCameraYawRad;
@@ -26,6 +34,9 @@ namespace FastDragon
         private const float CorrectionAnimationDuration = 1;
         private const float MinLoadingWaitTime = 1;
 
+        private const float GemPathBezierControlSpread = 20;
+        private const float GemSpawnGrowTime = 0.05f;
+
         private string _levelSceneFile;
         private string _prevousMapFile;
         private DirectionalLight3D _oldSun;
@@ -38,6 +49,11 @@ namespace FastDragon
 
         private bool _isReturningHome => _prevousMapFile != null;
         private Node3D _loadedScene;
+
+        private MeshLabel3D _untalliedGemsLabel => GetNode<MeshLabel3D>("%UntalliedGemsLabel");
+        private MeshLabel3D _talliedGemsLabel => GetNode<MeshLabel3D>("%TalliedGemsLabel");
+        private AnimationPlayer _labelSlider => GetNode<AnimationPlayer>("%LabelSlider");
+        private Dictionary<GemColor, int> _untalliedGems;
         private int _talliedGems;
 
         private StateMachine _stateMachine = new StateMachine(typeof(LoadingScreenState));
@@ -57,7 +73,7 @@ namespace FastDragon
         public void Initialize(
             string levelSceneFile,
             string previousMapFile,
-            Environment skyBoxEnvironment,
+            Godot.Environment skyBoxEnvironment,
             double animationStartTime,
             Vector3 playerStartRotRad,
             float cameraStartDist,
@@ -90,8 +106,38 @@ namespace FastDragon
             _camera.OrbitYawRad = cameraStartYawRad;
             _camera.OrbitPitchRad = cameraStartPitchRad;
 
+            // Figure out how many of each gem we'll need to spawn in the
+            // gem counting animation later.
+            _untalliedGems = SaveFile.Current.UntalliedGems;
+            SaveFile.Current.UntalliedGems = new Dictionary<GemColor, int>();
+            _talliedGems = SaveFile.Current.TotalGemCount - TotalUntalliedGems();
+
+            // Hide the labels until it's time for them to be seen
+            _untalliedGemsLabel.Visible = false;
+            _talliedGemsLabel.Visible = false;
+
             // Start the animation
             _stateMachine.ChangeState<MovingToRest>();
+        }
+
+        private int TotalUntalliedGems()
+        {
+            return _untalliedGems
+                .Select(kvp => (int)kvp.Key * kvp.Value)
+                .Sum();
+        }
+
+        private int TotalIndividualUntalliedGems()
+        {
+            return _untalliedGems
+                .Select(kvp => kvp.Value)
+                .Sum();
+        }
+
+        private void UpdateLabelText()
+        {
+            _untalliedGemsLabel.Text = $"Treasure found: {TotalUntalliedGems()}";
+            _talliedGemsLabel.Text = $"Total treasure: {_talliedGems}";
         }
 
         private void GoToTargetMap()
@@ -140,21 +186,6 @@ namespace FastDragon
 
         }
 
-        private int CalculateTalliedGems()
-        {
-            int totalUntallied = 0;
-
-            foreach (var kvp in SaveFile.Current.UntalliedGems)
-            {
-                GemColor color = kvp.Key;
-                int count = kvp.Value;
-
-                totalUntallied += (int)color * count;
-            }
-
-            return SaveFile.Current.TotalGemCount - totalUntallied;
-        }
-
         private abstract partial class LoadingScreenState : State
         {
             protected PortalLoadingScreen _screen => _stateMachine.GetParent<PortalLoadingScreen>();
@@ -171,67 +202,138 @@ namespace FastDragon
                 tween.Parallel().TweenProperty(_screen._camera, "OrbitDistance", CameraDist, RestMoveDuration);
                 tween.Parallel().TweenAngleRadSinusoidal(_screen._camera, "OrbitYawRad", _screen._cameraYawRad, RestMoveDuration);
                 tween.Parallel().TweenAngleRadSinusoidal(_screen._camera, "OrbitPitchRad", _screen._cameraPitchRad, RestMoveDuration);
-                tween.TweenCallback(Callable.From(() => ChangeState<CountingGems>()));
+                tween.TweenCallback(Callable.From(() => ChangeState<SlidingInLabels>()));
+            }
+        }
+
+        private partial class SlidingInLabels : LoadingScreenState
+        {
+            public override void OnStateEntered()
+            {
+                _screen._untalliedGemsLabel.Visible = true;
+                _screen._talliedGemsLabel.Visible = true;
+                _screen.UpdateLabelText();
+                _screen._labelSlider.Play("SlideIn");
+
+                // We just made the text visible, but it won't naturally jump to
+                // its starting position until the next frame when the animator
+                // processes.  This will result in the text appearing to "blink"
+                // for one frame before the animation actually starts.
+                //
+                // To avoid this, let's just force it to update right now.
+                _screen._labelSlider.Seek(0, true);
+            }
+
+            public override void _Process(double delta)
+            {
+                if (!_screen._labelSlider.IsPlaying())
+                {
+                    if (_screen.TotalUntalliedGems() > 0)
+                    {
+                        ChangeState<CountingGems>();
+                    }
+                    else
+                    {
+                        ChangeState<LettingPlayerReadLabels>();
+                    }
+                }
             }
         }
 
         private partial class CountingGems : LoadingScreenState
         {
-            private Label _untalliedGemsLabel => _screen.GetNode<Label>("%UntalliedGemsLabel");
-            private Label _talliedGemsLabel => _screen.GetNode<Label>("%TalliedGemsLabel");
+            private Node3D _gemSpawn => _screen.GetNode<Node3D>("%GemSpawn");
+            private Node3D _gemDest => _screen.GetNode<Node3D>("%GemDest");
 
-            private int _untalliedGems;
-            private int _talliedGems;
+            private Random _rng = new Random();
+
             private double _interval;
             private double _timer;
 
             public override void OnStateEntered()
             {
                 GD.Print("Started counting gems");
-
                 _timer = 0;
-                _talliedGems = _screen.CalculateTalliedGems();
-                _untalliedGems = SaveFile.Current.TotalGemCount - _talliedGems;
-                SaveFile.Current.UntalliedGems.Clear();
-
-                _untalliedGemsLabel.Visible = true;
-                _talliedGemsLabel.Visible = true;
-
-                _untalliedGemsLabel.Text = $"Treasure found: {_untalliedGems}";
-                _talliedGemsLabel.Text = $"Total treasure: {_talliedGems}";
-
-                _interval = CountingGemsDuration / _untalliedGems;
+                _interval = CountingGemsDuration / _screen.TotalIndividualUntalliedGems();
             }
 
             public override void _Process(double delta)
             {
                 _timer += delta;
 
-                if (_untalliedGems <= 0)
-                {
-                    ChangeState<WaitingForLoad>();
-                    return;
-                }
-
-                if (_timer >= _interval)
+                if (_screen.TotalUntalliedGems() > 0 && _timer >= _interval)
                 {
                     _timer -= _interval;
-                    _untalliedGems--;
-                    _talliedGems++;
 
-                    _untalliedGemsLabel.Text = $"Treasure found: {_untalliedGems}";
-                    _talliedGemsLabel.Text = $"Total treasure: {_talliedGems}";
+                    GemColor color = _rng.PickFromWeighted(_screen._untalliedGems);
+                    SpawnGem(color);
                 }
+            }
+
+            private void SpawnGem(GemColor value)
+            {
+                _screen._untalliedGems[value]--;
+                _screen.UpdateLabelText();
+
+                var gem = PrefabForGemColor(value).Instantiate<Node3D>();
+                _gemSpawn.GetParent().AddChild(gem);
+
+                gem.Position = _gemSpawn.Position;
+                gem.Scale = Vector3.Zero;
+
+                var tween = CreateTween();
+
+                tween.TweenVector3Bezier(
+                    gem,
+                    "position",
+                    _gemDest.Position,
+                    RandomBezierControlPoint(GemPathBezierControlSpread),
+                    CountingGemsDuration
+                );
+                tween.Parallel().TweenProperty(gem, "scale", Vector3.One, GemSpawnGrowTime);
+
+                tween.TweenCallback(Callable.From(() => CountGem(gem, value)));
+            }
+
+            private void CountGem(Node3D gem, GemColor value)
+            {
+                _screen._talliedGems += (int)value;
+                _screen.UpdateLabelText();
+
+                gem.QueueFree();
+
+                if (_screen._talliedGems >= SaveFile.Current.TotalGemCount)
+                    ChangeState<LettingPlayerReadLabels>();
+            }
+
+            private Vector3 RandomBezierControlPoint(float spread)
+            {
+                Vector3 center = _gemDest.Position;
+                float offset = (GD.Randf() - 0.5f) * spread;
+
+                return center + (Vector3.Right * offset);
+            }
+
+            private PackedScene PrefabForGemColor(GemColor color)
+            {
+                return color switch
+                {
+                    GemColor.Red => _screen.RedGemPrefab,
+                    GemColor.Green => _screen.GreenGemPrefab,
+                    GemColor.Purple => _screen.PurpleGemPrefab,
+                    GemColor.Yellow => _screen.YellowGemPrefab,
+                    GemColor.Magenta => _screen.MagentaGemPrefab,
+                    _ => throw new Exception("Invalid gem color chosen")
+                };
             }
         }
 
-        private partial class WaitingForLoad : LoadingScreenState
+        private partial class LettingPlayerReadLabels : LoadingScreenState
         {
             private double _timer;
 
             public override void OnStateEntered()
             {
-                GD.Print("Started waiting for loading to finish");
                 _timer = MinLoadingWaitTime;
             }
 
@@ -239,7 +341,20 @@ namespace FastDragon
             {
                 _timer -= delta;
 
-                if (_timer <= 0 && _screen._loadedScene != null)
+                if (_timer <= 0)
+                {
+                    _screen._labelSlider.PlayBackwards("SlideIn");
+                    ChangeState<WaitingForLoad>();
+                }
+            }
+        }
+
+        private partial class WaitingForLoad : LoadingScreenState
+        {
+
+            public override void _Process(double delta)
+            {
+                if (!_screen._labelSlider.IsPlaying() && _screen._loadedScene != null)
                     ChangeState<CorrectingAngle>();
             }
         }
