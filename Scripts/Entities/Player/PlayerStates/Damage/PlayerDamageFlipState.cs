3ac6779e9c2106f2047ce9f2134a15326c188d1f
Alex Shelley <alexthepeppershelley@gmail.com>
2023-12-29 11:01:41 -0500

    Add PlayerDamageFlipState

---
+++ Scripts/Entities/Player/PlayerStates/Damage/PlayerDamageFlipState.cs
@@ -0,0 +1,36 @@
+using Godot;
+
+namespace FastDragon
+{
+    public partial class PlayerDamageFlipState : PlayerState
+    {
+        private const float VSpeedBoost = 5;
+
+        public override void OnStateEntered()
+        {
+            _player.Animator.Play("DamageFlip");
+            _player.VSpeed += VSpeedBoost;
+        }
+
+        public override void _PhysicsProcess(double delta)
+        {
+            ApplyGravity((float)delta);
+            _player.MoveAndSlide();
+
+            if (!_player.Animator.IsPlaying())
+            {
+                int currentHealth = (int)SaveFile.Current.PlayerHealth;
+
+                if (currentHealth <= 0)
+                {
+                    // TODO: Choose a more appropriate death state
+                    _player.ChangeState<PlayerFallingToDeathState>();
+                }
+                else
+                {
+                    _player.ChangeState<PlayerWalkState>();
+                }
+            }
+        }
+    }
+}
