5c9513ce97b099ec17715559545ba9730719d7a3
Alex Shelley <alexthepeppershelley@gmail.com>
2023-12-30 15:15:14 -0500

    The portal exit animation no longer unlocks the camera

--- Scripts/Entities/Portal.cs
+++ Scripts/Entities/Portal.cs
@@ -37,7 +37,6 @@ namespace FastDragon
             player.GlobalPosition -= _playerSpawn.GlobalForward() * (player.Camera.OrbitDistance + 2);
             player.ResetPhysicsInterpolation();
 
-            player.Camera.DisableInput = false;
             player.Camera.OrbitYawRad = _playerSpawn.GlobalRotation.Y + Mathf.DegToRad(180);
             player.Camera.OrbitPitchRad = 0;
 
