ba560e86052cdaf21f4859f05afebb68784e9b58
Alex Shelley <alexthepeppershelley@gmail.com>
2023-12-30 14:34:42 -0500

    Remove the unused mouselook feature

--- Scripts/Entities/Player/CameraStates/OrbitCameraFreeState.cs
+++ Scripts/Entities/Player/CameraStates/OrbitCameraFreeState.cs
@@ -7,23 +7,10 @@ namespace FastDragon
         [Export] public float FollowDistance = 6;
         [Export] public float ZoomSpeed = 4;
 
-        [Export] public bool AllowMouseLook = false;
-        [Export] public float MouseSensitivity = 0.0001f;
         [Export] public float RightStickRotSpeedDeg = 180;
 
         [Export] public float MinOrbitPitchDeg = -89;
         [Export] public float MaxOrbitPitchDeg = 0;
-
-
-        public override void _Input(InputEvent ev)
-        {
-            if (ev is InputEventMouseMotion mouse && AllowMouseLook)
-            {
-                _camera.OrbitYawRad += mouse.Velocity.X * MouseSensitivity;
-                _camera.OrbitPitchRad += mouse.Velocity.Y * MouseSensitivity;
-                ClampOrbitAngles();
-            }
-        }
 
         public override void _Process(double deltaD)
         {
