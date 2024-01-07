4178a9c03128f4d0ca668c0830c14eeba8468618
Alex Shelley <ashelley@purdue.edu>
2024-01-07 14:10:11 -0500

    Add HandCannonBall projectile

---
+++ Scripts/Objects/Projectiles/HandCannonBall.cs
@@ -0,0 +1,47 @@
+using Godot;
+
+namespace FastDragon
+{
+    public partial class HandCannonBall : StaticBody3D
+    {
+        public float Speed = 10;
+
+        private Node3D _model => GetNode<Node3D>("%Model");
+        private GpuParticles3D _explosionParticles => GetNode<GpuParticles3D>("%ExplosionParticles");
+
+        private bool _destroyed = false;
+
+        public override void _Ready()
+        {
+            SignalBus.Instance.LevelReset += QueueFree;
+        }
+
+        public override void _PhysicsProcess(double deltaD)
+        {
+            float delta = (float)deltaD;
+
+            if (_destroyed)
+            {
+                if (!_explosionParticles.Emitting)
+                    QueueFree();
+
+                return;
+            }
+
+            var velocity = this.GlobalForward() * Speed;
+            var collision = MoveAndCollide(velocity * delta);
+
+            if (collision == null)
+                return;
+
+            _destroyed = true;
+            _model.Visible = false;
+            _explosionParticles.Emitting = true;
+
+            if (collision.GetCollider() is Player p)
+            {
+                p.TryDamage<PlayerDamageFlipState>();
+            }
+        }
+    }
+}
