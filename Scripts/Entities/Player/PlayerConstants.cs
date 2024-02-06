74af2be29e233714b276ee9f931b8e0936159468
Alex Shelley <alexthepeppershelley@gmail.com>
2024-02-06 16:18:07 -0500

    Remove the concept of 'Spyro Units'

--- Scripts/Entities/Player/PlayerConstants.cs
+++ Scripts/Entities/Player/PlayerConstants.cs
@@ -4,43 +4,23 @@ namespace FastDragon
 {
     public partial class Player
     {
-        /// <summary>
-        /// Conversion factor between Godot distance units and Spyro distance
-        /// units.
-        ///
-        /// The player in this game has a diameter of 1 Godot unit.
-        /// I measured Spyro's diameter to be about 500 Spyro units.
-        /// In theory, this should mean the conversion factor is 1f / 500, but
-        /// for some reason, that makes everything feel too fast.
-        /// Using 1f / 550 makes everything feel more accurate.
-        /// Perhaps I measured Spyro's diameter wrong?  Who knows.
-        /// </summary>
-        private const float SpyroUnits = 1f / 550;
-        private const float SpyroAnglesToDeg = 360f / 4096;
-        private const float SpyroFrames = 1f / 30;
-        private const float SpyroUnitsPerFrame = SpyroUnits / SpyroFrames;
-        private const float SpyroUnitsPerFrameSquared = SpyroUnits / (SpyroFrames * SpyroFrames);
-        private const float SpyroAnglesPerFrame = SpyroAnglesToDeg / SpyroFrames;
-
         public static class Default
         {
-            public const float Gravity = 12 * SpyroUnitsPerFrameSquared;
+            public const float Gravity = 19.63636363636364f;
         }
 
         public static class Stand
         {
-            // The real game uses a value of 180, but I'd like to make _this_
-            // game feel a little bit more responsive than that.
-            public const float RotSpeedDeg = 270 * SpyroAnglesPerFrame;
+            public const float RotSpeedDeg = 711.9140625f;
         }
 
         public static class Walk
         {
-            public const float MinSpeed = 15 * SpyroUnitsPerFrame;
-            public const float Speed = 143 * SpyroUnitsPerFrame;
-            public const float Accel = 20 * SpyroUnitsPerFrameSquared;
-            public const float Decel = 12 * SpyroUnitsPerFrameSquared;
-            public const float RotSpeedDeg = 80 * SpyroAnglesPerFrame;
+            public const float MinSpeed = 0.8181818181818182f;
+            public const float Speed = 7.8f;
+            public const float Accel = 32.72727272727273f;
+            public const float Decel = 19.63636363636364f;
+            public const float RotSpeedDeg = 210.9375f;
         }
 
         public static class Kick
@@ -75,16 +55,16 @@ namespace FastDragon
             // boost.  #NotSorry.
             public const float RotSpeedDeg = 720;
 
-            public const float MaxFSpeed = 100 * SpyroUnitsPerFrame;
+            public const float MaxFSpeed = 5.454545454545455f;
             public const float StrafeAccel = 20;
 
 
-            public const float FullJumpHeight = 1300 * SpyroUnits;
-            public const float FullJumpRiseTime = 15 * SpyroFrames;
+            public const float FullJumpHeight = 2.363636363636364f;
+            public const float FullJumpRiseTime = 0.5f;
             public static readonly float FullJumpRiseGravity;
 
-            public const float MinJumpHeight = 530 * SpyroUnits;
-            public const float MinJumpRiseTime = 9 * SpyroFrames;
+            public const float MinJumpHeight = 0.9636363636363636f;
+            public const float MinJumpRiseTime = 0.3f;
             public static readonly float ShortHopGravity;
 
             public static readonly float InitVSpeed;
@@ -102,7 +82,7 @@ namespace FastDragon
 
         public static class Dive
         {
-            public const float TurnSpeedDeg = 48 * SpyroAnglesPerFrame;
+            public const float TurnSpeedDeg = 126.5625f;
 
             public const float FSpeed = 12;
             public const float InitialVSpeed = 12;
@@ -128,7 +108,7 @@ namespace FastDragon
 
         public static class Glide
         {
-            public const float MaxFSpeed = 200 * SpyroUnitsPerFrame;
+            public const float MaxFSpeed = 10.90909090909091f;
             public const float CameraDistance = 7;
         }
     }
