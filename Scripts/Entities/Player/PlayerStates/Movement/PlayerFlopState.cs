a41cc0b554c7a61f6d125d7d1dfad59c392421f9
Alex Shelley <alexthepeppershelley@gmail.com>
2024-02-07 19:45:05 -0500

    The flop state now gives you the same freedom of control as the jump state

--- Scripts/Entities/Player/PlayerStates/Movement/PlayerFlopState.cs
+++ Scripts/Entities/Player/PlayerStates/Movement/PlayerFlopState.cs
@@ -16,11 +16,24 @@ namespace FastDragon
                 _player.ChangeState<PlayerDiveState>();
                 return;
             }
+
+            if (InputService.KickJustPressed(ev))
+            {
+                _player.ChangeState<PlayerKickState>();
+                return;
+            }
         }
 
         public override void _PhysicsProcess(double deltaD)
         {
             float delta = (float)deltaD;
+
+            RotateTowardLeftStick(Player.Jump.RotSpeedRad, delta);
+            AccelerateWithLeftStick(
+                Player.Jump.MaxFSpeed,
+                Player.Jump.StrafeAccel,
+                delta
+            );
 
             ApplyGravity(delta, Player.Default.Gravity);
             _player.MoveAndSlide();
@@ -30,6 +43,9 @@ namespace FastDragon
                 _player.ChangeState<PlayerWalkState>();
                 return;
             }
+
+            if (TryGrabLedge())
+                return;
 
             if (_player.IsOnWall() && _player.VSpeed < 0)
             {
