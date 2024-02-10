6125dba4e4e538c97659df6ce3a3afefac7657fb
Alex Shelley <alexthepeppershelley@gmail.com>
2024-02-10 16:23:14 -0500

    The portal loading screen now looks more suited to a human, rather than a dragon

--- Scripts/Entities/Portal.cs
+++ Scripts/Entities/Portal.cs
@@ -9,11 +9,15 @@ namespace FastDragon
         [Export(PropertyHint.File)] public string TargetMap;
 
         [Export] public float ExitAnimationDuration = 1.5f;
-        [Export] public float ExitAnimationStartHeight = 2;
+        [Export] public float ExitAnimationStartHeight = 0;
+        [Export] public float ExitAnimationParabolaHeight = 2;
 
         private Node3D _playerSpawn => GetNode<Node3D>("%PlayerSpawnPoint");
-
         private PortalSurface _surface => GetNode<PortalSurface>("%PortalSurface");
+
+        private Vector3 _exitAnimationStartPos;
+        private float _exitAnimationTimer;
+        private bool _playingExitAnimation = false;
 
         public override void _Ready()
         {
@@ -24,40 +28,54 @@ namespace FastDragon
         public void PlayExitAnimation()
         {
             var player = GetTree().FindNode<Player>();
-
-            player.AllowInterpolation = false;
+            player.Animator.Play(
+                "Roll",
+                customBlend: 0.5f,
+                customSpeed: 2
+            );
 
             // Warp the player to the start pos of the animation
+            _exitAnimationStartPos = _playerSpawn.GlobalPosition;
+            _exitAnimationStartPos += Vector3.Up * ExitAnimationStartHeight;
+            _exitAnimationStartPos -= _playerSpawn.GlobalForward() * (player.Camera.OrbitDistance + 2);
+
             player.ChangeState<PlayerManhandledState>();
             player.GlobalRotation = _playerSpawn.GlobalRotation;
-            player.GlobalPosition = _playerSpawn.GlobalPosition;
-            player.GlobalPosition += Vector3.Up * ExitAnimationStartHeight;
-            player.GlobalPosition -= _playerSpawn.GlobalForward() * (player.Camera.OrbitDistance + 2);
+            player.GlobalPosition = _exitAnimationStartPos;
             player.ResetPhysicsInterpolation();
 
             player.Camera.OrbitYawRad = _playerSpawn.GlobalRotation.Y + Mathf.DegToRad(180);
             player.Camera.OrbitPitchRad = 0;
 
             // Start tweening the player to the spawn point
-            var tween = CreateTween();
-            tween.TweenProperty(
-                player,
-                "global_position",
-                _playerSpawn.GlobalPosition,
-                ExitAnimationDuration
-            );
-
-            tween.TweenCallback(Callable.From(FinishExitAnimation));
+            _exitAnimationTimer = 0;
+            _playingExitAnimation = true;
         }
 
-        private void FinishExitAnimation()
+        public override void _PhysicsProcess(double deltaD)
         {
-            var player = GetTree().FindNode<Player>();
-            player.AllowInterpolation = true;
-            player.ResetPhysicsInterpolation();
-            player.ForceUpdateTransform();
+            float delta = (float)deltaD;
 
-            player.ChangeState<PlayerWalkState>();
+            if (!_playingExitAnimation)
+                return;
+
+            _exitAnimationTimer += delta;
+            float t = _exitAnimationTimer / ExitAnimationDuration;
+
+            var player = GetTree().FindNode<Player>();
+            player.GlobalPosition = _exitAnimationStartPos.LerpParabola(
+                _playerSpawn.GlobalPosition,
+                ExitAnimationParabolaHeight,
+                t
+            );
+
+            if (_exitAnimationTimer > ExitAnimationDuration)
+            {
+                _playingExitAnimation = false;
+                player.GlobalPosition = _playerSpawn.GlobalPosition;
+                player.ResetPhysicsInterpolation();
+                player.ChangeState<PlayerWalkState>();
+            }
         }
     }
 }
