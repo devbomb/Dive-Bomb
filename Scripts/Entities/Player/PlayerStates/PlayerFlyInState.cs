a0f0b612460e764b27add540036a454e57467bbc
Alex Shelley <alexthepeppershelley@gmail.com>
2024-02-19 14:32:38 -0500

    Rewrite PlayerFlyInState to not use a tween, because that's been causing all sorts of problems

--- Scripts/Entities/Player/PlayerStates/PlayerFlyInState.cs
+++ Scripts/Entities/Player/PlayerStates/PlayerFlyInState.cs
@@ -7,40 +7,62 @@ namespace FastDragon
         public override bool Invincible => true;
         public override bool DisableCameraInput => true;
 
+        private Vector3 _startPos;
+        private Vector3 _startRotRad;
+
+        private Vector3 _endPos;
+        private Vector3 _endRotRad;
+        private float _timer;
+
         public override void OnStateEntered()
         {
-            Vector3 endPos = _player.GlobalPosition;
-            Vector3 endRotRad = _player.GlobalRotation;
+            _endPos = _player.GlobalPosition;
+            _endRotRad = _player.GlobalRotation;
+            _timer = 0;
 
             _player.Animator.Play(
                 "Roll",
                 customBlend: 0.25f,
                 customSpeed: 2);
-            _player.AllowInterpolation = false;
 
             _player.GlobalPosition += Vector3.Up * _player.FlyInHeight;
             _player.GlobalPosition -= _player.GlobalForward() * _player.FlyInDistance;
             _player.GlobalRotation = Vector3.Zero;
             _player.ResetPhysicsInterpolation();
 
+            _startPos = _player.GlobalPosition;
+            _startRotRad = _player.GlobalRotation;
+
             _player.Camera.OrbitDistance = PortalLoadingScreen.CameraDist;
             _player.Camera.OrbitYawRad = PortalLoadingScreen.EnterLevelCameraYawRad;
             _player.Camera.OrbitPitchRad = PortalLoadingScreen.EnterLevelCameraPitchRad;
-
-            double duration = _player.FlyInDuration;
-            var tween = CreateTween();
-            tween.TweenInterval(0.5);   // HACK: Pause for a bit to hide the "frame hiccup"
-            tween.TweenProperty(_player, "global_position", endPos, duration);
-            tween.Parallel().TweenProperty(_player, "global_rotation", endRotRad, duration);
-            tween.Parallel().TweenAngleRadSinusoidal(_player.Camera, "OrbitYawRad", endRotRad.Y, duration);
-            tween.Parallel().TweenAngleRadSinusoidal(_player.Camera, "OrbitPitchRad", 0, duration);
-            tween.TweenCallback(Callable.From(Finish));
         }
 
-        private void Finish()
+        public override void _PhysicsProcess(double deltaD)
         {
-            _player.AllowInterpolation = true;
-            _player.Respawn();
+            float delta = (float)deltaD;
+
+            _timer += delta;
+            float t = _timer / _player.FlyInDuration;
+            _player.GlobalPosition = _startPos.Lerp(_endPos, t);
+            _player.GlobalRotation = _startRotRad.LerpEulerRadSinusoidal(_endRotRad, t);
+
+            _player.Camera.OrbitYawRad = Mathf.LerpAngle(
+                PortalLoadingScreen.EnterLevelCameraYawRad,
+                _endRotRad.Y,
+                t
+            );
+
+            _player.Camera.OrbitPitchRad = Mathf.LerpAngle(
+                PortalLoadingScreen.EnterLevelCameraPitchRad,
+                0,
+                t
+            );
+
+            if (_timer > _player.FlyInDuration)
+            {
+                _player.Respawn();
+            }
         }
     }
 }
