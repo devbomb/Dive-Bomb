3d1c6cd97978b5dfcf47f9d2001a6f6d15e5dd4f
Alex Shelley <alexthepeppershelley@gmail.com>
2024-02-27 16:47:05 -0500

    Add a basic fairy scene.  It doesn't have an fgd yet, nor does its collectedness get saved.

---
+++ Scripts/Entities/Fairy.cs
@@ -0,0 +1,94 @@
+using System.Linq;
+using Godot;
+
+namespace FastDragon
+{
+    public partial class Fairy : Node3D
+    {
+        private readonly StateMachine _stateMachine = new StateMachine(typeof(FairyState));
+        private Area3D PlayerDetector => GetNode<Area3D>("%PlayerDetector");
+
+        private Player Player;
+
+        public override void _Ready()
+        {
+            SignalBus.Instance.LevelReset += Reset;
+            AddChild(_stateMachine);
+            Reset();
+        }
+
+        public void Reset()
+        {
+            _stateMachine.ChangeState<Idle>();
+        }
+
+        private partial class Idle : FairyState
+        {
+            public override void OnStateEntered()
+            {
+                _fairy.Visible = true;
+                _fairy.PlayerDetector.BodyEntered += OnBodyEntered;
+            }
+
+            public override void OnStateExited()
+            {
+                _fairy.PlayerDetector.BodyEntered -= OnBodyEntered;
+            }
+
+            private void OnBodyEntered(Node3D body)
+            {
+                if (body is Player player)
+                {
+                    _fairy.Player = player;
+                    ChangeState<Rescuing>();
+                }
+            }
+        }
+
+        private partial class Rescuing : FairyState
+        {
+            private float _timer;
+
+            public override void OnStateEntered()
+            {
+                _fairy.Player.ChangeState<PlayerManhandledState>();
+                _fairy.Player.GlobalRotation = _fairy.Player
+                    .GlobalPosition
+                    .DirectionTo(_fairy.GlobalPosition)
+                    .Flattened()
+                    .Normalized()
+                    .ForwardToEulerAnglesRad();
+                _fairy.Player.ResetPhysicsInterpolation();
+
+                _timer = 3;
+            }
+
+            public override void OnStateExited()
+            {
+                _fairy.Player?.ChangeState<PlayerWalkState>();
+                _fairy.Player = null;
+            }
+
+            public override void _PhysicsProcess(double deltaD)
+            {
+                _timer -= (float)deltaD;
+
+                if (_timer <= 0)
+                    ChangeState<Rescued>();
+            }
+        }
+
+        private partial class Rescued : FairyState
+        {
+            public override void OnStateEntered()
+            {
+                _fairy.Visible = false;
+            }
+        }
+
+        private abstract partial class FairyState : State
+        {
+            protected Fairy _fairy => _stateMachine.GetParent<Fairy>();
+        }
+    }
+}
