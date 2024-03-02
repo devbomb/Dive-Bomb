d6f4bf32bb65c37ef3476875faa940cf9a453cea
Alex Shelley <alexthepeppershelley@gmail.com>
2024-03-02 14:57:26 -0500

    Make it run _after_ entities have been imported

--- addons/MapImporter/map_importer.gd
+++ addons/MapImporter/map_importer.gd
@@ -52,7 +52,8 @@ func _get_import_options(path, preset_index):
             return []
 
 func _get_import_order():
-    return 100
+    return 101 # Make it process _after_ .tscn files, to ensure the entities
+               # have been imported before we try to instantiate them.
 
 func _get_option_visibility(path, option_name, options):
     return true
