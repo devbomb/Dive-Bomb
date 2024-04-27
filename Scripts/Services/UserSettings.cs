b64f7bbe8941a4395136796a76a2685e4bb64ccd
Alex Shelley <alexthepeppershelley@gmail.com>
2024-04-27 12:41:47 -0400

    Add a UserSettings service, with a ShowPerformanceStats field

---
+++ Scripts/Services/UserSettings.cs
@@ -0,0 +1,46 @@
+using Newtonsoft.Json;
+using Godot;
+
+namespace FastDragon
+{
+    public class UserSettings
+    {
+        private const string FilePath = "user://UserSettings.json";
+
+        public static UserSettings Instance { get; } = LoadFromJson();
+
+        public bool ShowPerformanceStats = false;
+
+        public void SaveToJson()
+        {
+            string json = JsonConvert.SerializeObject(
+                this,
+                new JsonSerializerSettings { Formatting = Formatting.Indented }
+            );
+
+            using var file = FileAccess.Open(FilePath, FileAccess.ModeFlags.Write);
+            file.StoreLine(json);
+            file.Close();
+        }
+
+        private static UserSettings LoadFromJson()
+        {
+            if (!FileAccess.FileExists(FilePath))
+                return new UserSettings();
+
+            try
+            {
+                using var file = FileAccess.Open(FilePath, FileAccess.ModeFlags.Read);
+                string json = file.GetAsText();
+                file.Close();
+
+                return JsonConvert.DeserializeObject<UserSettings>(json);
+            }
+            catch (JsonException err)
+            {
+                GD.PushWarning($"Error parsing UserSettings.json.  Using default settings.\n{err}");
+                return new UserSettings();
+            }
+        }
+    }
+}
