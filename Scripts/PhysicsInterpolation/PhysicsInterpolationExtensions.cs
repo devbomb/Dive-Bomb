a496890bbf4788807cdb367d1e67441c0ed02166
Alex Shelley <alexthepeppershelley@gmail.com>
2024-05-09 18:51:49 -0400

    Add IsPhysicsInterpolated and SetPhysicsInterpolated

--- Scripts/PhysicsInterpolation/PhysicsInterpolationExtensions.cs
+++ Scripts/PhysicsInterpolation/PhysicsInterpolationExtensions.cs
@@ -8,5 +8,18 @@ namespace FastDragon
         {
             PhysicsInterpolatorSingleton.Instance.ResetPhysicsInterpolation(node);
         }
+
+        public static bool IsPhysicsInterpolated(this Node3D node)
+        {
+            return node.IsInGroup(PhysicsInterpolatorSingleton.GroupName);
+        }
+
+        public static void SetPhysicsInterpolated(this Node3D node, bool value)
+        {
+            if (value)
+                node.AddToGroup(PhysicsInterpolatorSingleton.GroupName);
+            else
+                node.RemoveFromGroup(PhysicsInterpolatorSingleton.GroupName);
+        }
     }
 }
