79e6cda068125179b4c68589ce50907cd96e3d8c
Alex Shelley <alexthepeppershelley@gmail.com>
2024-05-13 16:55:42 -0400

    Add BreakableArea3D and BreakableStaticBody3D

---
+++ Scripts/Objects/BreakableStaticBody3D.cs
@@ -0,0 +1,33 @@
+using Godot;
+
+namespace FastDragon
+{
+    public partial class BreakableStaticBody3D : Area3D, IBreakable
+    {
+        [Signal] public delegate void KickedEventHandler();
+        [Signal] public delegate void RolledIntoEventHandler();
+        [Signal] public delegate void BrokenEventHandler();
+
+        [Export] public bool Rollable { get; set; } = true;
+        [Export] public bool Kickable { get; set; } = true;
+        [Export] public bool Disabled { get; set; } = false;
+
+        public bool VulnerableToRoll => Rollable && !Disabled;
+        public bool VulnerableToKick => Kickable && !Disabled;
+
+        public void OnKicked()
+        {
+            EmitSignal(SignalName.Kicked);
+        }
+
+        public void OnRolledInto()
+        {
+            EmitSignal(SignalName.RolledInto);
+        }
+
+        public void OnBroken()
+        {
+            EmitSignal(SignalName.Broken);
+        }
+    }
+}
