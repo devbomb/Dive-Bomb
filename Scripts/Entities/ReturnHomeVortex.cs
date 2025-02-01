a83f06eb3061c83b3fc646150f0775ea057fc15a
Alex Shelley <alexthepeppershelley@gmail.com>
2025-02-01 13:04:18 -0500

    Rewrite ReturnHomeVortex to use a state machine

--- Scripts/Entities/ReturnHomeVortex.cs
+++ Scripts/Entities/ReturnHomeVortex.cs
@@ -4,62 +4,90 @@ namespace FastDragon
 {
     public partial class ReturnHomeVortex : Node3D
     {
-        private const float AscendDuration = 3.75f;
-        private const float RotSpeedDeg = 180;
-
         [Export] public float ExitHeight = 15;
 
-        private Player _ensnaredPlayer = null;
+        private readonly StateMachine _stateMachine = new StateMachine(typeof(VortexState));
 
         public override void _Ready()
         {
-            SignalBus.Instance.LevelReset += OnLevelReset;
+            SignalBus.Instance.LevelReset += Reset;
+            AddChild(_stateMachine);
+
+            Reset();
         }
 
-        public override void _PhysicsProcess(double deltaD)
+        private void Reset()
         {
-            float delta = (float)deltaD;
-
-            if (_ensnaredPlayer == null)
-                return;
-
-            // Spin the player and move them up
-            float speed = ExitHeight / AscendDuration;
-            _ensnaredPlayer.GlobalPosition += Vector3.Up * speed * delta;
-            _ensnaredPlayer.GlobalRotationDegrees += Vector3.Up * RotSpeedDeg * delta;
-
-            // Rotate the camera underneath the player
-            _ensnaredPlayer.Camera.OrbitPitchRad = AngleMath.DecayToward(
-                _ensnaredPlayer.Camera.OrbitPitchRad,
-                Mathf.DegToRad(89.999f),
-                2,
-                delta
-            );
-
-            bool playerReachedExitHeight = _ensnaredPlayer.GlobalPosition.Y >= GlobalPosition.Y + ExitHeight;
-            bool isTimeTrialMode = GetTree().FindNode<TimeTrialManager>()?.IsTimeTrialMode ?? false;
-            if (playerReachedExitHeight && !isTimeTrialMode)
-            {
-                MapTransitionManager.Instance.ExitLevel();
-            }
+            _stateMachine.ChangeState<ReadyState>();
         }
 
         public void OnTriggerBodyEntered(Node body)
         {
-            if (body is Player p && !(p.CurrentState is PlayerManhandledState))
+            var state = (VortexState)_stateMachine.CurrentState;
+            state.OnTriggerBodyEntered(body);
+        }
+
+        private abstract partial class VortexState : State
+        {
+            protected ReturnHomeVortex _vortex => _stateMachine.GetParent<ReturnHomeVortex>();
+            public virtual void OnTriggerBodyEntered(Node body) {}
+        }
+
+        private partial class ReadyState : VortexState
+        {
+            public override void OnTriggerBodyEntered(Node body)
             {
-                _ensnaredPlayer = p;
-                p.ChangeState<PlayerManhandledState>();
-                p.Animator.Play("Glide");
+                if (body is Player p && !(p.CurrentState is PlayerManhandledState))
+                    ChangeState<ExitingLevelState>();
+            }
+        }
+
+        private partial class ExitingLevelState : VortexState
+        {
+            private const float AscendDuration = 3.75f;
+            private const float RotSpeedDeg = 180;
+
+            private Player _player;
+
+            public override void OnStateEntered()
+            {
+                _player = GetTree().FindNode<Player>();
+                _player.ChangeState<PlayerManhandledState>();
+                _player.Animator.Play("Glide");
 
                 TimeTrialSaveData.Instance.UnlockAnyPercent(SaveFile.Current.CurrentMap);
                 GetTree().FindNode<TimeTrialManager>()?.Finish();
             }
-        }
 
-        private void OnLevelReset()
-        {
-            _ensnaredPlayer = null;
+            public override void _PhysicsProcess(double deltaD)
+            {
+                float delta = (float)deltaD;
+
+                if (_player == null)
+                    return;
+
+                // Spin the player and move them up
+                float speed = _vortex.ExitHeight / AscendDuration;
+                _player.GlobalPosition += Vector3.Up * speed * delta;
+                _player.GlobalRotationDegrees += Vector3.Up * RotSpeedDeg * delta;
+
+                // Rotate the camera underneath the player
+                _player.Camera.OrbitPitchRad = AngleMath.DecayToward(
+                    _player.Camera.OrbitPitchRad,
+                    Mathf.DegToRad(89.999f),
+                    2,
+                    delta
+                );
+
+                float h = _vortex.GlobalPosition.Y + _vortex.ExitHeight;
+                bool playerReachedExitHeight = _player.GlobalPosition.Y >= h;
+                bool isTimeTrialMode = GetTree().FindNode<TimeTrialManager>()?.IsTimeTrialMode ?? false;
+
+                if (playerReachedExitHeight && !isTimeTrialMode)
+                {
+                    MapTransitionManager.Instance.ExitLevel();
+                }
+            }
         }
     }
 }
