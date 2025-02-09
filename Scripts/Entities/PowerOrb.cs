d75ba8156cbdb021984b341404ced2091ffe4598
Alex Shelley <alexthepeppershelley@gmail.com>
2025-02-09 17:12:27 -0500

    Give the orbs a spawning animation

--- Scripts/Entities/PowerOrb.cs
+++ Scripts/Entities/PowerOrb.cs
@@ -5,10 +5,13 @@ namespace FastDragon
     // TODO: Don't use inheritance, ya twit!
     public partial class PowerOrb : BreakableStaticBody3D
     {
+        [Export] public float SpawnTime = 0.25f;
+
         public bool IsBroken => _stateMachine.CurrentState is BrokenState;
 
         private Node3D _model => GetNode<Node3D>("%Model");
         private CollisionShape3D _bodyShape => GetNode<CollisionShape3D>("%BodyShape");
+        private Node3D _blobShadow => GetNode<Node3D>("%BlobShadow");
 
         private readonly StateMachine _stateMachine = new StateMachine(typeof(PowerOrbState));
 
@@ -29,7 +32,7 @@ namespace FastDragon
 
         public void Reveal()
         {
-            _stateMachine.ChangeState<Revealed>();
+            _stateMachine.ChangeState<Revealing>();
         }
 
         public void SetHidden()
@@ -47,6 +50,42 @@ namespace FastDragon
             protected PowerOrb _self => _stateMachine.GetParent<PowerOrb>();
         }
 
+        private partial class Revealing : PowerOrbState
+        {
+            private float _animTimer;
+            private float _timer;
+
+            public override void OnStateEntered()
+            {
+                _self._bodyShape.Disabled = true;
+                _self._model.Scale = Vector3.Zero;
+                _timer = 0;
+                _animTimer = 0;
+            }
+
+            public override void OnStateExited()
+            {
+                _self._bodyShape.Disabled = false;
+                _self._model.Scale = Vector3.One;
+            }
+
+            public override void _Process(double deltaD)
+            {
+                _animTimer += (float)deltaD;
+
+                float t = _animTimer / _self.SpawnTime;
+                _self._model.Scale = Vector3.Zero.Lerp(Vector3.One, t);
+            }
+
+            public override void _PhysicsProcess(double deltaD)
+            {
+                _timer += (float)deltaD;
+
+                if (_timer >= _self.SpawnTime)
+                    ChangeState<Revealed>();
+            }
+        }
+
         private partial class Revealed : PowerOrbState
         {
         }
@@ -57,12 +96,14 @@ namespace FastDragon
             {
                 _self._bodyShape.Disabled = true;
                 _self._model.Visible = false;
+                _self._blobShadow.Visible = false;
             }
 
             public override void OnStateExited()
             {
                 _self._bodyShape.Disabled = false;
                 _self._model.Visible = true;
+                _self._blobShadow.Visible = true;
             }
         }
 
@@ -73,12 +114,14 @@ namespace FastDragon
                 // TODO: Play particle effects
                 _self._bodyShape.Disabled = true;
                 _self._model.Visible = false;
+                _self._blobShadow.Visible = false;
             }
 
             public override void OnStateExited()
             {
                 _self._bodyShape.Disabled = false;
                 _self._model.Visible = true;
+                _self._blobShadow.Visible = true;
             }
         }
     }
