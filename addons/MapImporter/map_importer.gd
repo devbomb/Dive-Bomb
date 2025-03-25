b913edea3f77fca0044ad42fbe007a6202250a3f
Alex Shelley <alexthepeppershelley@gmail.com>
2025-03-24 20:18:29 -0400

    Add type hints to MapImporter

--- addons/MapImporter/map_importer.gd
+++ addons/MapImporter/map_importer.gd
@@ -3,35 +3,35 @@ extends EditorImportPlugin
 
 enum Presets { DEFAULT }
 
-func _get_importer_name():
+func _get_importer_name() -> String:
 	return "map_importer"
 
-func _get_visible_name():
+func _get_visible_name() -> String:
 	return "Map Importer"
 
-func _get_priority():
+func _get_priority() -> float:
 	return 2
 
-func _get_recognized_extensions():
+func _get_recognized_extensions() -> PackedStringArray:
 	return ["map"]
 
-func _get_save_extension():
+func _get_save_extension() -> String:
 	return "scn"
 
-func _get_resource_type():
+func _get_resource_type() -> String:
 	return "PackedScene"
 
-func _get_preset_count():
+func _get_preset_count() -> int:
 	return Presets.size()
 
-func _get_preset_name(preset_index):
+func _get_preset_name(preset_index) -> String:
 	match preset_index:
 		Presets.DEFAULT:
 			return "Default"
 		_:
 			return "Unknown"
 
-func _get_import_options(path, preset_index):
+func _get_import_options(path, preset_index) -> Array[Dictionary]:
 	match preset_index:
 		Presets.DEFAULT:
 			return [
@@ -61,14 +61,19 @@ func _get_import_options(path, preset_index):
 		_:
 			return []
 
-func _get_import_order():
+func _get_import_order() -> int:
 	return 101 # Make it process _after_ .tscn files, to ensure the entities
 			   # have been imported before we try to instantiate them.
 
 func _get_option_visibility(path, option_name, options):
 	return true
 
-func _import(source_file, save_path, options, r_platform_variants, r_gen_files):
+func _import(
+	source_file: String,
+	save_path: String,
+	options: Dictionary,
+	r_platform_variants: Array[String],
+	r_gen_files: Array[String]):
 	print("Importing " + source_file)
 	var tbLoader: TBLoader = TBLoader.new()
 
