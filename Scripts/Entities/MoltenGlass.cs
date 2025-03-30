308a3ccb88a6d959f2d587fda7916e67d8130c83
Alex Shelley <alexthepeppershelley@gmail.com>
2025-03-30 18:02:59 -0400

    Add a molten glass brush (with a cool shader!) that kills you.

---
+++ Scripts/Entities/MoltenGlass.cs
@@ -0,0 +1,18 @@
+using Godot;
+
+namespace FastDragon
+{
+    public partial class MoltenGlass : Area3D
+    {
+        public override void _Ready()
+        {
+            BodyEntered += OnBodyEntered;
+        }
+
+        public void OnBodyEntered(Node3D body)
+        {
+            if (body is Player)
+                MapTransitionManager.Instance.RespawnPlayerAfterDeath();
+        }
+    }
+}
