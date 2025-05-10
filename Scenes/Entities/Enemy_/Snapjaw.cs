c245a12ebb3b0522a8f7bfd6b28c7c240628f085
Alex Shelley <alexthepeppershelley@gmail.com>
2025-05-10 16:24:34 -0400

    Move the snapjaw through code instead of the animation

--- Scenes/Entities/Enemy_/Snapjaw.cs
+++ Scenes/Entities/Enemy_/Snapjaw.cs
@@ -5,20 +5,40 @@ namespace FastDragon
 {
     public partial class Snapjaw : Node3D
     {
+        private RayCast3D _floorDetector => GetNode<RayCast3D>("%FloorDetector");
         private AnimationTree _animator => GetNode<AnimationTree>("%AnimationTree");
         private readonly StateMachine _stateMachine = new StateMachine(typeof(SnapjawState));
+
+        private Vector3 _targetPos;
+        private Vector3 _floorPos;
+        private bool _foundFloorPos = false;
 
         public override void _Ready()
         {
             AddChild(_stateMachine);
             SignalBus.Instance.LevelReset += Reset;
 
+            _targetPos = GlobalPosition;
+
+            // HACK: We can't do the raycast in _Ready() because the floor may
+            // not have been added to the tree yet.  Instead, we'll wait until
+            // the first frame to find it.
+            _floorPos = GlobalPosition;
+            Callable.From(() =>
+            {
+                _floorDetector.ForceRaycastUpdate();
+                _floorPos = _floorDetector.GetCollisionPoint();
+                _floorPos.Y += 1;
+
+                _floorDetector.Enabled = false;
+            }).CallDeferred();
+
             Reset();
         }
 
         private void Reset()
         {
-            _stateMachine.ChangeState<Looping>();
+            _stateMachine.ChangeState<Watching>();
         }
 
         public void OnBroken()
@@ -31,11 +51,107 @@ namespace FastDragon
             protected Snapjaw _self => _stateMachine.GetParent<Snapjaw>();
         }
 
-        private partial class Looping : SnapjawState
+        private partial class Watching : SnapjawState
         {
+            private const double Duration = 2;
+
+            private double _timer;
+
             public override void OnStateEntered()
             {
+                _self._animator.PlayState("Watch");
+                _timer = Duration;
+            }
+
+            public override void _PhysicsProcess(double delta)
+            {
+                _self.GlobalPosition = _self._floorPos;
+                _self.ResetPhysicsInterpolation3D();
+
+                _timer -= delta;
+                if (_timer <= 0)
+                    ChangeState<Attacking>();
+            }
+        }
+
+        private partial class Attacking : SnapjawState
+        {
+            private double _duration;
+            private double _timer;
+
+            public override void OnStateEntered()
+            {
+                _duration = _self._animator.GetAnimPlayer().GetAnimation("Attack").Length;
+
+                _timer = 0;
+                _self.GlobalPosition = _self._floorPos;
+                _self.ResetPhysicsInterpolation3D();
+
                 _self._animator.PlayState("Attack");
+            }
+
+            public override void _PhysicsProcess(double delta)
+            {
+                _timer += delta;
+                float t = (float)(_timer / _duration);
+
+                _self.GlobalPosition = _self._floorPos.Lerp(_self._targetPos, t);
+
+                if (_timer >= _duration)
+                    ChangeState<Hovering>();
+            }
+        }
+
+        private partial class Hovering : SnapjawState
+        {
+            private const double Duration = 0.75;
+
+            private double _timer;
+
+            public override void OnStateEntered()
+            {
+                _timer = Duration;
+                _self.GlobalPosition = _self._targetPos;
+            }
+
+            public override void _PhysicsProcess(double delta)
+            {
+                _timer -= delta;
+                if (_timer <= 0)
+                    ChangeState<Falling>();
+            }
+        }
+
+        private partial class Falling : SnapjawState
+        {
+            private const double Duration = 1;
+
+            private double _timer;
+
+            public override void OnStateEntered()
+            {
+                _timer = 0;
+                _self.GlobalPosition = _self._targetPos;
+                _self._animator.GetAnimPlayer().SpeedScale = (float)(1.0 / Duration);
+
+                _self._animator.PlayState("Fall");
+            }
+
+            public override void OnStateExited()
+            {
+                _self._animator.GetAnimPlayer().SpeedScale = 1;
+            }
+
+            public override void _PhysicsProcess(double delta)
+            {
+                _timer += delta;
+                float t = (float)(_timer / Duration);
+                // TODO: Massage "t" so it looks like gravity
+
+                _self.GlobalPosition = _self._targetPos.Lerp(_self._floorPos, t);
+
+                if (_timer >= Duration)
+                    ChangeState<Watching>();
             }
         }
 
