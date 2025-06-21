acbeb49482dd6e32ebadce67da2d20e43b6a746a
Alex Shelley <alexthepeppershelley@gmail.com>
2025-06-21 17:13:14 -0400

    Add DivableWall.  That was tricky!

---
+++ FuncGodotAssets/EntityDefinitions/Solid/Wall/wall_divable.gd
@@ -0,0 +1,21 @@
+@tool
+extends StaticBody3D
+
+var scene: PackedScene = preload("res://Scenes/Entities/Wall_/DivableWall.tscn")
+
+func _ready():
+	replace.call_deferred()
+
+func replace():
+	# Move all the geometry generated for this node into a replacement coming
+	# from the scene.
+	var replacement_node: Node3D = scene.instantiate()
+	replacement_node.transform = transform
+	
+	while (get_child_count() > 0):
+		var child: Node = get_child(0)
+		remove_child(child)
+		replacement_node.add_child(child)
+		
+	get_parent().add_child(replacement_node)
+	queue_free()
