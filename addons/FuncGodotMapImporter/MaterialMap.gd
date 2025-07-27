2aeb22f645e9772c60f0cb9d3efb8d89a8a97116
Alex Shelley <alexthepeppershelley@gmail.com>
2025-07-27 12:42:40 -0400

    MaterialMap now wraps its dictionary accesses in function calls

--- addons/FuncGodotMapImporter/MaterialMap.gd
+++ addons/FuncGodotMapImporter/MaterialMap.gd
@@ -1,3 +1,10 @@
+@tool
 class_name MaterialMap extends Resource
 
 @export var texture_to_material: Dictionary[Texture2D, Material] = {}
+
+func has_material_for(texture: Texture2D) -> bool:
+	return texture_to_material.has(texture)
+
+func get_material_for(texture: Texture2D) -> Material:
+	return texture_to_material[texture]
