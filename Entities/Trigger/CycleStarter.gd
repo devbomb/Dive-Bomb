6786a150be3330505cb4b2ed84bcdc54d4dd93f9
Alex Shelley <alexthepeppershelley@gmail.com>
2025-08-23 17:57:32 -0400

    Add a safety timer to cycle starters

--- Entities/Trigger/CycleStarter.gd
+++ Entities/Trigger/CycleStarter.gd
@@ -2,8 +2,25 @@ extends Area3D
 
 @export var CycleId: String
 
-func _ready():
-	connect("body_entered", _on_body_entered)
+var _safety_timer: int
+var _started: bool
 
-func _on_body_entered(body: Node3D) -> void:
-	$"/root/SignalBus".EmitCycleStarted(CycleId)
+func _ready() -> void:
+	$/root/SignalBus.connect("LevelReset", _reset)
+	_reset()
+
+func _reset() -> void:
+	_safety_timer = 2
+	_started = false
+
+func _physics_process(_delta) -> void:
+	# HACK: If the level resets while the player is inside the trigger, they will
+	# still be consider "inside" the trigger for a few more frames.  To avoid
+	# this, wait a few frames after the reset before checking.
+	if (_safety_timer > 0):
+		_safety_timer -= 1
+		return
+
+	if (!_started && !get_overlapping_bodies().is_empty()):
+		_started = true
+		$"/root/SignalBus".EmitCycleStarted(CycleId)
