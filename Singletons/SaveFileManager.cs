378d92cb7cd23a17b4e66b8e43b41cc37a7713e6
Alex Shelley <alexthepeppershelley@gmail.com>
2025-09-05 18:20:44 -0400

    Show which level each file was saved in

--- Singletons/SaveFileManager.cs
+++ Singletons/SaveFileManager.cs
@@ -31,15 +31,30 @@ namespace FastDragon
             return FileAccess.FileExists(SlotFilePath(slotNumber));
         }
 
-        public void LoadFromSlot(int slotNumber)
+        /// <summary>
+        /// Returns the save data located in the given slot without technically
+        /// "loading" it as the current file.  Useful if you want to display a
+        /// summary of this save file in a menu.
+        ///
+        /// Returns null if there is no save file in this slot.
+        /// </summary>
+        public SaveFile PeekSlot(int slotNumber)
         {
+            if (!SlotHasData(slotNumber))
+                return null;
+
             string filePath = SlotFilePath(slotNumber);
             using var file = FileAccess.Open(filePath, FileAccess.ModeFlags.Read);
             string json = file.GetAsText();
             file.Close();
 
+            return SaveFile.FromJson(json);
+        }
+
+        public void LoadFromSlot(int slotNumber)
+        {
             ActiveSlot = slotNumber;
-            CurrentFile = SaveFile.FromJson(json);
+            CurrentFile = PeekSlot(slotNumber);
 
             LevelTransitionManager.Instance.GoToLevelWithFadeToBlack(CurrentFile.CurrentLevel);
         }
--- UI/Menus/SaveSlotManagementMenu/SaveSlotManagementMenu.cs
+++ UI/Menus/SaveSlotManagementMenu/SaveSlotManagementMenu.cs
@@ -1,11 +1,26 @@
 using Godot;
 using System;
+using System.Linq;
 
 namespace FastDragon
 {
     public partial class SaveSlotManagementMenu : Page
     {
         [Export(PropertyHint.File, hintString: "*.tscn")] public string NewGameLevel;
+
+        public override void OnPageEntered()
+        {
+            var buttons = GetNode<Control>("%SlotButtons")
+                .EnumerateChildren()
+                .Cast<Button>()
+                .ToArray();
+
+            for (int i = 0; i < buttons.Length; i++)
+            {
+                buttons[i].Text = SlotText(i);
+            }
+        }
+
         public void OnSlotChosen(int slotNumber)
         {
             if (SaveFileManager.Instance.SlotHasData(slotNumber))
@@ -16,6 +31,20 @@ namespace FastDragon
             {
                 SaveFileManager.Instance.StartNewGame(slotNumber, NewGameLevel);
             }
+        }
+
+        private string SlotText(int slotNumber)
+        {
+            if (!SaveFileManager.Instance.SlotHasData(slotNumber))
+                return "New Game";
+
+            // Peek at the save file to learn which level it was saved in
+            var saveFile = SaveFileManager.Instance.PeekSlot(slotNumber);
+
+            string levelScenePath = saveFile.CurrentLevel;
+            string levelName = AtlasCache.Instance.GetEntry(levelScenePath).HumanReadableName;
+
+            return $"Continue -- {levelName}";
         }
     }
 }
