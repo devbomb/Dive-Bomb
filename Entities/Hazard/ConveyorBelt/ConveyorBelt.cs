43ecccf52f916c0cf484b7cabae588a158d93786
Alex Shelley <alexthepeppershelley@gmail.com>
2025-09-20 17:16:42 -0400

    Directions are now projected onto the conveyor belt's "top" face

--- Entities/Hazard/ConveyorBelt/ConveyorBelt.cs
+++ Entities/Hazard/ConveyorBelt/ConveyorBelt.cs
@@ -10,8 +10,19 @@ namespace FastDragon
         [Export] public float Speed = 10;
         [Export] public string DirectionMarkerId;
 
+        private Vector3 _normal;
+
         public override void _Ready()
         {
+            // Find the normal of the "top" that the player would stand on.
+            // The metadata we're searching will have been added by func_godot
+            // when the map was imported.
+            _normal = GetMeta("func_godot_mesh_data")
+                .AsGodotDictionary()["normals"]
+                .AsVector3Array()
+                .OrderBy(n => n.Y)
+                .Last();
+
             Callable.From(SetDirection).CallDeferred();
         }
 
@@ -25,18 +36,18 @@ namespace FastDragon
             if (marker == null)
                 throw new Exception($"Could not find conveyor belt direction marker with ID \"{DirectionMarkerId}\"");
 
-            var direction = marker.GlobalForward();
+            var direction = marker
+                .GlobalForward()
+                .ProjectOnPlane(_normal)
+                .Normalized();
+
             ConstantLinearVelocity = direction * Speed;
 
+            var scrollSpeed = new Vector2(direction.X, direction.Z).Normalized() * Speed;
             foreach (var meshInstance in this.EnumerateDescendantsOfType<MeshInstance3D>())
             {
-                meshInstance.SetInstanceShaderParameter("scroll_speed", GetScrollSpeed());
+                meshInstance.SetInstanceShaderParameter("scroll_speed", scrollSpeed);
             }
-        }
-
-        private Vector2 GetScrollSpeed()
-        {
-            return new Vector2(0, Speed);
         }
     }
 }
