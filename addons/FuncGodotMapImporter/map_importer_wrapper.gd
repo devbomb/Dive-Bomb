86f6c5313222c22ed47f8873c7c87fa965593ad1
Alex Shelley <alexthepeppershelley@gmail.com>
2025-10-05 20:10:54 -0400

    The tool now preserves tracks whose RESET value doesn't match the rest pose

--- addons/FuncGodotMapImporter/map_importer_wrapper.gd
+++ addons/FuncGodotMapImporter/map_importer_wrapper.gd
@@ -35,6 +35,10 @@ func _delete_unnecessary_player_animation_tracks():
 	var library: AnimationLibrary = ResourceLoader.load("res://Entities/Player/KennifiedPlayerAnimations.tres")
 	var reset_animation: Animation = library.get_animation("RESET")
 	
+	var player_model_scene: PackedScene = ResourceLoader.load("res://Entities/Player/Parts/PlayerModel/KennifiedPlayerModel.blend")
+	var player_model: Node3D = player_model_scene.instantiate()
+	var skeleton: Skeleton3D = player_model.get_node("Armature/Skeleton3D")
+	
 	for animation_name in library.get_animation_list():
 		if (animation_name == "RESET"):
 			continue
@@ -43,10 +47,10 @@ func _delete_unnecessary_player_animation_tracks():
 		if (animation.is_built_in()):
 			continue
 			
-		_delete_unnecessary_tracks_from(animation, reset_animation)
+		_delete_unnecessary_tracks_from(animation, reset_animation, skeleton)
 		ResourceSaver.save(animation, animation.resource_path)
 
-func _delete_unnecessary_tracks_from(animation: Animation, reset_animation: Animation) -> void:
+func _delete_unnecessary_tracks_from(animation: Animation, reset_animation: Animation, skeleton: Skeleton3D) -> void:
 	print("Deleting unnecessary tracks from " + animation.resource_path)
 	
 	# Iterating the tracks in reverse so the remaining track indices don't shift
@@ -54,8 +58,6 @@ func _delete_unnecessary_tracks_from(animation: Animation, reset_animation: Anim
 	var track_idxs = range(animation.get_track_count())
 	track_idxs.reverse()
 	for track_idx in track_idxs:
-		if (animation.track_get_key_count(track_idx) > 1):
-			continue
 			
 		var track_path = animation.track_get_path(track_idx)
 		var track_type = animation.track_get_type(track_idx)
@@ -69,12 +71,41 @@ func _delete_unnecessary_tracks_from(animation: Animation, reset_animation: Anim
 		if (disallowed_types.has(track_type)):
 			continue
 		
+		# Don't delete if it doesn't have a RESET value
 		var reset_track_idx: int = reset_animation.find_track(track_path, track_type)
 		if (reset_track_idx == -1):
 			print("!!! " + str(track_path) + "(" + str(track_type) + ") doesn't have a RESET value")
 			continue
-
+		
+		# Don't delete if its value isn't the same as its RESET value
+		if (animation.track_get_key_count(track_idx) > 1):
+			continue
+			
 		var reset_track_value: Variant = reset_animation.track_get_key_value(reset_track_idx, 0)
-		if (track_value == reset_track_value):
-			print("Deleting track " + str(track_path) + "(" + str(track_type) + ")")
-			animation.remove_track(track_idx)
+		if (track_value != reset_track_value):
+			continue
+		
+		# Don't delete if it has a rest pose which doesn't match the RESET value
+		const bone_prefix = "KennifiedPlayerModel/Armature/Skeleton3D:"
+		if (str(track_path).begins_with(bone_prefix)):
+			var bone_name = str(track_path).trim_prefix(bone_prefix)
+			var bone_idx = skeleton.find_bone(bone_name)
+			if (bone_idx == -1):
+				continue
+				
+			var rest_pose = skeleton.get_bone_rest(bone_idx)
+			
+			match (track_type):
+				Animation.TrackType.TYPE_POSITION_3D:
+					if (!rest_pose.origin.is_equal_approx(track_value)):
+						continue
+				Animation.TrackType.TYPE_ROTATION_3D:
+					if (!rest_pose.basis.get_rotation_quaternion().is_equal_approx(track_value)):
+						continue
+				Animation.TrackType.TYPE_SCALE_3D:
+					if (!rest_pose.basis.get_scale().is_equal_approx(track_value)):
+						continue
+		
+		# OK, fine, you can delete it
+		print("Deleting track " + str(track_path) + "(" + str(track_type) + ")")
+		animation.remove_track(track_idx)
