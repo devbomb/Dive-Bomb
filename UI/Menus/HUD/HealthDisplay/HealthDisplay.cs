fa732124526a7e93d8d7111fe369463be0a8b842
Alex Shelley <alexthepeppershelley@gmail.com>
2025-10-25 20:00:32 -0400

    Completely rewrite the health circle code in C#, and make it smoothly drain!

---
+++ UI/Menus/HUD/HealthDisplay/HealthDisplay.cs
@@ -0,0 +1,130 @@
+using System;
+using System.Collections.Generic;
+using Godot;
+
+namespace FastDragon
+{
+    public partial class HealthDisplay : Control
+    {
+        [Export] public TextureProgressBar ProgressBar;
+        [Export] public Control RestPoint;
+        [Export] public Control CenterPoint;
+
+        private int _lastHealth;
+
+        private readonly StateMachine _stateMachine = new();
+
+        public HealthDisplay()
+        {
+            AddChild(_stateMachine);
+        }
+
+        public override void _Ready()
+        {
+            SignalBus.Instance.LevelReset += Reset;
+            Reset();
+        }
+
+        private void Reset()
+        {
+            _lastHealth = SaveFileManager.Current.PlayerHealth;
+            _stateMachine.ChangeState<Resting>();
+        }
+
+        public override void _Process(double delta)
+        {
+            if (_lastHealth != SaveFileManager.Current.PlayerHealth)
+            {
+                _lastHealth = SaveFileManager.Current.PlayerHealth;
+                _stateMachine.ChangeState<Draining>();
+            }
+        }
+
+        private void UpdatePosition(float t)
+        {
+            ProgressBar.AnchorLeft = Mathf.Lerp(RestPoint.AnchorLeft, CenterPoint.AnchorLeft, t);
+            ProgressBar.AnchorRight = Mathf.Lerp(RestPoint.AnchorRight, CenterPoint.AnchorRight, t);
+            ProgressBar.AnchorTop = Mathf.Lerp(RestPoint.AnchorTop, CenterPoint.AnchorTop, t);
+            ProgressBar.AnchorBottom = Mathf.Lerp(RestPoint.AnchorBottom, CenterPoint.AnchorBottom, t);
+
+            ProgressBar.OffsetLeft = Mathf.Lerp(RestPoint.OffsetLeft, CenterPoint.OffsetLeft, t);
+            ProgressBar.OffsetRight = Mathf.Lerp(RestPoint.OffsetRight, CenterPoint.OffsetRight, t);
+            OffsetTop = Mathf.Lerp(RestPoint.OffsetTop, CenterPoint.OffsetTop, t);
+            OffsetBottom = Mathf.Lerp(RestPoint.OffsetBottom, CenterPoint.OffsetBottom, t);
+        }
+
+        private class Resting : State<HealthDisplay>
+        {
+            public override void OnStateEntered()
+            {
+                Self.UpdatePosition(0);
+                Self.ProgressBar.Value = SaveFileManager.Current.PlayerHealth;
+            }
+        }
+
+        private class Draining : State<HealthDisplay>
+        {
+            private const double DrainDelay = 0.3;
+            private const double DrainDuration = 0.125;
+            private const double HangDuration = 0.5;
+            private const double ReturnDuration = 0.5;
+
+            private Coroutine _coroutine;
+
+            public override void OnStateEntered()
+            {
+                _coroutine = new Coroutine(CoroutineExecutor());
+                Self.UpdatePosition(1);
+            }
+
+            public override void OnStateExited()
+            {
+                Self.UpdatePosition(0);
+            }
+
+            public override void _Process(double delta)
+            {
+                _coroutine.Tick(delta);
+            }
+
+            private IEnumerator<YieldInstruction> CoroutineExecutor()
+            {
+                yield return Coroutine.WaitSeconds(DrainDelay);
+                yield return Coroutine.WaitFor(Drain());
+                yield return Coroutine.WaitSeconds(HangDuration);
+                yield return Coroutine.WaitFor(Return());
+
+                ChangeState<Resting>();
+            }
+
+            private IEnumerator<YieldInstruction> Drain()
+            {
+                double startValue = Self.ProgressBar.Value;
+
+                for (double timer = 0; timer <= DrainDuration; timer += Self.GetProcessDeltaTime())
+                {
+                    double targetValue = SaveFileManager.Current.PlayerHealth;
+                    double t = Math.Min(timer / DrainDuration, 1.0);
+                    t *= t;
+
+                    Self.ProgressBar.Value = Mathf.Lerp(startValue, targetValue, t);
+                    yield return default;
+                }
+
+                Self.ProgressBar.Value = SaveFileManager.Current.PlayerHealth;
+            }
+
+            private IEnumerator<YieldInstruction> Return()
+            {
+                for (double timer = 0; timer <= ReturnDuration; timer += Self.GetProcessDeltaTime())
+                {
+                    double t = 1.0 - Math.Min(timer / ReturnDuration, 1.0);
+                    t *= t;
+
+                    Self.UpdatePosition((float)t);
+                    yield return default;
+                }
+            }
+        }
+    }
+}
