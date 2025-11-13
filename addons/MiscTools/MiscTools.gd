ae32adf6f29d314997cb9f680fdb589407feb1a0
Alex Shelley <alexthepeppershelley@gmail.com>
2025-11-13 15:29:06 -0500

    Add a script that prompts you for a level ID and creates the environment file for it

--- addons/MiscTools/MiscTools.gd
+++ addons/MiscTools/MiscTools.gd
@@ -2,9 +2,32 @@
 extends EditorPlugin
 
 func _enter_tree() -> void:
-	add_tool_menu_item("Delete all unnecessary player animation tracks", _delete_unnecessary_player_animation_tracks)
+	add_tool_menu_item("Create new level", execute_create_new_level)
+	add_tool_menu_item("Delete all unnecessary player animation tracks", execute_delete_unnecessary_player_animation_tracks)
 
-func _delete_unnecessary_player_animation_tracks():
+func execute_create_new_level() -> void:
+	var level_id: String = await _prompt_string("Enter a level id(no spaces)")
+	if (level_id.is_empty()):
+		return
+	
+	_create_new_level(level_id, "res://Levels")
+	
+func _create_new_level(level_id: String, parent_folder: String) -> void:
+	var level_folder: String = parent_folder.path_join(level_id)
+	DirAccess.make_dir_absolute(level_folder)
+	
+	# Create a placeholder skybox
+	var environment = Environment.new()
+	ResourceSaver.save(environment, level_folder.path_join(level_id + "Skybox.tres"))
+	
+	# TODO: Create a maps folder, along with a "hazardous waste containment"
+	# folder for all of those pesky autosaves Trenchbroom likes to create.
+	
+	# Refresh the editor so the new folder can be seen
+	get_editor_interface().get_resource_filesystem().scan()
+	
+
+func execute_delete_unnecessary_player_animation_tracks():
 	var library: AnimationLibrary = ResourceLoader.load("res://Entities/Player/KennifiedPlayerAnimations.tres")
 	var reset_animation: Animation = library.get_animation("RESET")
 	
@@ -82,3 +105,51 @@ func _delete_unnecessary_tracks_from(animation: Animation, reset_animation: Anim
 		# OK, fine, you can delete it
 		print("Deleting track " + str(track_path) + "(" + str(track_type) + ")")
 		animation.remove_track(track_idx)
+
+# Returns the string the user typed.  Returns the empty string if the dialog was canceled
+func _prompt_string(prompt_text: String) -> String:
+	var stack_panel = VBoxContainer.new()
+	stack_panel.set_anchors_preset(Control.PRESET_FULL_RECT)
+	stack_panel.offset_top = 10
+	stack_panel.offset_left = 10
+	stack_panel.offset_bottom = -10
+	stack_panel.offset_right = -10
+	stack_panel.alignment = BoxContainer.ALIGNMENT_CENTER
+	
+	var textbox = LineEdit.new()
+	stack_panel.add_spacer(false)
+	stack_panel.add_child(textbox)
+	
+	var buttons_panel = HBoxContainer.new()
+	stack_panel.add_spacer(false)
+	stack_panel.add_child(buttons_panel)
+	buttons_panel.size_flags_horizontal = Control.SIZE_SHRINK_CENTER
+	
+	var ok_button = Button.new()
+	buttons_panel.add_child(ok_button)
+	ok_button.text = "OK"
+	ok_button.custom_minimum_size.x = 50
+	ok_button.pressed.connect(func (): 
+		textbox.text_submitted.emit(textbox.text)
+	)
+	
+	var cancel_button = Button.new()
+	buttons_panel.add_child(cancel_button)
+	cancel_button.text = "Cancel"
+	cancel_button.custom_minimum_size.x = 50
+	cancel_button.pressed.connect(func (): 
+		textbox.text_submitted.emit("")
+	)
+	
+	var window = Window.new()
+	window.title = prompt_text
+	window.add_child(stack_panel)
+	window.close_requested.connect(func ():
+		textbox.text_submitted.emit("")
+	)
+
+	get_editor_interface().popup_dialog_centered(window, Vector2i(500, 100))
+	var result: String = await textbox.text_submitted
+	window.queue_free()
+	
+	return result
