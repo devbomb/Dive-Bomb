e31fd991fd6ad790eee36e3b639d27cd5c614b54
Alex Shelley <alexthepeppershelley@gmail.com>
2025-11-23 20:42:54 -0500

    Fairy doors now have a short opening animation

--- Entities/Gate/FairyDoor/FairyDoor.cs
+++ Entities/Gate/FairyDoor/FairyDoor.cs
@@ -59,8 +59,6 @@ namespace FastDragon
         {
             public override void OnStateEntered()
             {
-                Self.Model.Visible = true;
-                Self.CollisionShape.Disabled = false;
                 Self.SoftlockFailsafeTrigger.BodyEntered += OnSoftlockFailsafeTriggerEntered;
             }
 
@@ -72,12 +70,49 @@ namespace FastDragon
             public override void _PhysicsProcess(double delta)
             {
                 if (Self.IsFairyFree())
-                    ChangeState<Open>();
+                    ChangeState<Opening>();
             }
 
             private void OnSoftlockFailsafeTriggerEntered(Node3D body)
             {
-                ChangeState<Open>();
+                ChangeState<Opening>();
+            }
+        }
+
+        private class Opening : State<FairyDoor>
+        {
+            private const double Duration = 0.5;
+
+            private Vector3 _startPos;
+            private Vector3 _targetPos;
+            private double _timer;
+
+            public override void OnStateEntered()
+            {
+                var shape = (BoxShape3D)Self.CollisionShape.Shape;
+                float height = shape.Size.Y;
+
+                _startPos = Self.GlobalPosition;
+                _targetPos = _startPos + (Vector3.Down * height);
+                _timer = 0;
+            }
+
+            public override void OnStateExited()
+            {
+                Self.GlobalPosition = _startPos;
+                Self.ResetPhysicsInterpolation3D();
+            }
+
+            public override void _PhysicsProcess(double delta)
+            {
+                _timer += delta;
+                float t = (float)(_timer / Duration);
+                t *= t;
+
+                Self.GlobalPosition = _startPos.Lerp(_targetPos, t);
+
+                if (_timer >= Duration)
+                    ChangeState<Open>();
             }
         }
 
@@ -87,6 +122,12 @@ namespace FastDragon
             {
                 Self.Model.Visible = false;
                 Self.CollisionShape.Disabled = true;
+            }
+
+            public override void OnStateExited()
+            {
+                Self.Model.Visible = true;
+                Self.CollisionShape.Disabled = false;
             }
         }
     }
