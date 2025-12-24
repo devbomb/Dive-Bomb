a41053fddc3df879e6bfb0935832069037ab670e
Alex Shelley <alexthepeppershelley@gmail.com>
2025-12-24 14:44:56 -0500

    Add GetNamedMarker3D() extension

--- Entities/Info/NamedMarker/NamedMarker3D.cs
+++ Entities/Info/NamedMarker/NamedMarker3D.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using Godot;
 
 namespace FastDragon
@@ -5,5 +6,16 @@ namespace FastDragon
     public partial class NamedMarker3D : Node3D
     {
         [Export] public string MarkerId;
+    }
+
+    public static class NamedMarker3DNodeExtensions
+    {
+        public static NamedMarker3D GetNamedMarker3D(this Node node, string markerId)
+        {
+            return node.GetTree()
+                .Root
+                .EnumerateDescendantsOfType<NamedMarker3D>()
+                .FirstOrDefault(m => m.MarkerId == markerId);
+        }
     }
 }
