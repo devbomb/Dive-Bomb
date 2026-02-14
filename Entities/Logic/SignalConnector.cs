1bb55f34c941727a877c3ba9bf0a9801c642401a
Alex Shelley <alexthepeppershelley@gmail.com>
2026-02-13 19:32:26 -0500

    Attempt to create logic_signal_connector, but I got it wrong.

---
+++ Entities/Logic/SignalConnector.cs
@@ -0,0 +1,17 @@
+using System;
+using Godot;
+
+namespace FastDragon
+{
+    public partial class SignalConnector : Node3D
+    {
+        [Export] public NodePath target;
+        [Export] public string Signal;
+
+        [Export] public NodePath killtarget;
+        [Export] public string HandlerFunction;
+
+        public NodePath From => target;
+        public NodePath To => killtarget;
+    }
+}
