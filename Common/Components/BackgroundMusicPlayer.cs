ff5066f225ad80c3db03e4d4035bd171bc10b38b
Alex Shelley <alexthepeppershelley@gmail.com>
2026-06-13 17:01:56 -0400

    BackgroundMusicPlayer now delays for a short time before starting the music

--- Common/Components/BackgroundMusicPlayer.cs
+++ Common/Components/BackgroundMusicPlayer.cs
@@ -5,6 +5,10 @@ namespace FastDragon
     [GlobalClass]
     public partial class BackgroundMusicPlayer : AudioStreamPlayer
     {
+        [Export] public double StartDelaySeconds = 0.25;
+        private double _startDelayTimer;
+        private bool _delayingStart = false;
+
         public override void _Ready()
         {
             SignalBus.Instance.ExitReached += Stop;
@@ -15,8 +19,27 @@ namespace FastDragon
 
         private void Reset()
         {
-            if (!Playing)
-                Play();
+            // HACK: Delay starting the music to avoid a bug where the music
+            // briefly starts during the fly-in animation when it's not supposed
+            // to.
+            //
+            // TODO: Find a more elegant way to solve this.
+            Stop();
+            _delayingStart = true;
+            _startDelayTimer = StartDelaySeconds;
+        }
+
+        public override void _PhysicsProcess(double delta)
+        {
+            if (_delayingStart)
+            {
+                _startDelayTimer -= delta;
+                if (_startDelayTimer <= 0)
+                {
+                    _delayingStart = false;
+                    Play();
+                }
+            }
         }
     }
 }
