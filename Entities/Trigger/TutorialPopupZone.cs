72010577b75fcba07fcb04339030e3797412838c
Alex Shelley <alexthepeppershelley@gmail.com>
2026-06-20 15:00:19 -0400

    Add the fading effect

--- Entities/Trigger/TutorialPopupZone.cs
+++ Entities/Trigger/TutorialPopupZone.cs
@@ -6,6 +6,10 @@ namespace FastDragon
     public partial class TutorialPopupZone : Area3D
     {
         [Export] public string Text = "";
+        [Export] public double FadeInDuration = 0.25;
+        [Export] public double FadeOutDuration = 0.5;
+
+        private bool _showing;
 
         private readonly RichTextLabel _label = new()
         {
@@ -40,12 +44,37 @@ namespace FastDragon
 
         public override void _Ready()
         {
+            SignalBus.Instance.LevelReset += Reset;
             _label.Text = Text;
+        }
+
+        private void Reset()
+        {
+            _showing = false;
+
+            var color = _label.Modulate;
+            color.A = 0;
+            _label.Modulate = color;
         }
 
         public override void _PhysicsProcess(double delta)
         {
-            _label.Visible = GetOverlappingBodies().Any();
+            _showing = GetOverlappingBodies().Any();
+        }
+
+        public override void _Process(double delta)
+        {
+            float targetAlpha = _showing
+                ? 1f
+                : 0f;
+
+            float speed = _showing
+                ? 1f / (float)FadeInDuration
+                : 1f / (float)FadeOutDuration;
+
+            var color = _label.Modulate;
+            color.A = Mathf.MoveToward(color.A, targetAlpha, speed * (float)delta);
+            _label.Modulate = color;
         }
     }
 }
