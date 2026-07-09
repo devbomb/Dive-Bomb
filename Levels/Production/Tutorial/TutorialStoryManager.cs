5dbed5d8249ecea87bb8c77e101ed3b61cf8db94
Alex Shelley <alexthepeppershelley@gmail.com>
2026-07-08 21:58:28 -0400

    Move the Dr. Monocle speech's music and story flag logic to TutorialStoryManager

--- Levels/Production/Tutorial/TutorialStoryManager.cs
+++ Levels/Production/Tutorial/TutorialStoryManager.cs
@@ -4,9 +4,13 @@ namespace FastDragon.Levels.Tutorial
 {
     public partial class TutorialStoryManager : Node
     {
+        [Export] public string targetname;
+
         [Export] public BackgroundMusicPlayer MusicPlayer;
         [Export] public AgentDIntroCutscene AgentDIntro;
         [Export] public DrMonocleIntroSpeechCutscene DrMonocleIntro;
+
+        private AudioStreamPlaybackInteractive _musicPlayback => (AudioStreamPlaybackInteractive)MusicPlayer.GetStreamPlayback();
 
         private static class StoryFlags
         {
@@ -15,6 +19,8 @@ namespace FastDragon.Levels.Tutorial
         }
 
         private readonly StateMachine _stateMachine = new();
+
+        private event System.Action _startDrMonocleSpeechRequested;
 
         public TutorialStoryManager()
         {
@@ -33,7 +39,7 @@ namespace FastDragon.Levels.Tutorial
             // cutscenes reset.
             Callable.From(() =>
             {
-                if (this.IsTimeTrialMode() || this.PlaySceneFromHereWasUsed())
+                if (this.IsTimeTrialMode())
                 {
                     _stateMachine.ChangeState<SneakyTime>();
                     return;
@@ -53,13 +59,21 @@ namespace FastDragon.Levels.Tutorial
                     .PermanentStoryFlags
                     .Contains(StoryFlags.AgentDIntroFinished);
 
-                if (!seenAgentDIntro)
+                if (!seenAgentDIntro && !this.PlaySceneFromHereWasUsed())
                 {
                     _stateMachine.ChangeState<PlayingAgentDIntro>();
                     return;
                 }
+
+                _stateMachine.ChangeState<SneakyTime>();
             }).CallDeferred();
         }
+
+        public void RequestStartDrMonocleSpeech(Node3D body)
+        {
+            _startDrMonocleSpeechRequested?.Invoke();
+        }
+
 
         private partial class PlayingAgentDIntro : State<TutorialStoryManager>
         {
@@ -100,17 +114,71 @@ namespace FastDragon.Levels.Tutorial
 
         private partial class SneakyTime : State<TutorialStoryManager>
         {
+            public override void OnStateEntered()
+            {
+                if (Self.MusicPlayer.Playing)
+                {
+                    Self._musicPlayback?.SwitchToClipByName("Normal");
+                }
 
+                Self.DrMonocleIntro.GoToIdle();
+            }
+
+            public override void SubscribeToSignals()
+            {
+                Self._startDrMonocleSpeechRequested += StartDrMonocleSpeech;
+            }
+
+            public override void UnsubscribeFromSignals()
+            {
+                Self._startDrMonocleSpeechRequested -= StartDrMonocleSpeech;
+            }
+
+            private void StartDrMonocleSpeech()
+            {
+                ChangeState<PlayingDrMonocleSpeech>();
+            }
         }
 
         private partial class PlayingDrMonocleSpeech : State<TutorialStoryManager>
         {
+            public override void OnStateEntered()
+            {
+                Self.MusicPlayer.Stop();
+                Self.DrMonocleIntro.StartPlaying();
+            }
 
+            public override void _PhysicsProcess(double delta)
+            {
+                if (!Self.DrMonocleIntro.IsPlaying)
+                    ChangeState<EscapeSequence>();
+            }
         }
 
         private partial class EscapeSequence : State<TutorialStoryManager>
         {
+            public override void OnStateEntered()
+            {
+                GD.Print("Starting escape sequence");
+                Self.MusicPlayer.Play();
+                Self._musicPlayback.SwitchToClipByName("Escape");
+            }
 
+            public override void SubscribeToSignals()
+            {
+                SignalBus.Instance.CheckpointActivated += CheckpointActivated;
+            }
+
+            public override void UnsubscribeFromSignals()
+            {
+                SignalBus.Instance.CheckpointActivated -= CheckpointActivated;
+            }
+
+            private void CheckpointActivated()
+            {
+                GD.Print("Dr. Monocle speech checkpointed");
+                Self.GetLevel().TempStoryFlags.Add(StoryFlags.DrMonocleSpeechCheckpointed);
+            }
         }
     }
 }
