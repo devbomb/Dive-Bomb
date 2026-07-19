e9358a5a28975f6946e65d129a35494eda4d9879
Alex Shelley <alexthepeppershelley@gmail.com>
2026-07-19 10:47:27 -0400

    The bonk decal now fades out after a bit

--- Entities/Player/Parts/BonkDecal/BonkDecal.cs
+++ Entities/Player/Parts/BonkDecal/BonkDecal.cs
@@ -1,9 +1,15 @@
+using System;
 using Godot;
 
 namespace FastDragon
 {
     public partial class BonkDecal : Node3D
     {
+        public const float FadeSpeed = 0.75f;
+
+        [ExportCategory("Internal")]
+        [Export] public Decal Decal;
+
         public override void _Ready()
         {
             SignalBus.Instance.LevelReset += Reset;
@@ -20,6 +26,16 @@ namespace FastDragon
             GlobalRotation = collision.GetNormal().ForwardToEulerAnglesRad();
             GlobalPosition = collision.GetPosition() + (collision.GetNormal() * 0.1f);
             Visible = true;
+            Decal.AlbedoMix = 1;
+        }
+
+        public override void _Process(double delta)
+        {
+            Decal.AlbedoMix = Mathf.MoveToward(
+                Decal.AlbedoMix,
+                0,
+                FadeSpeed * (float)delta
+            );
         }
     }
 }
