28f82429a4e17c11f52b26807a84e155509e0ad6
Alex Shelley <alexthepeppershelley@gmail.com>
2026-08-09 16:46:33 -0400

    Add a debug flag to LevelManifest

--- Common/Scripts/DataModel/Static/LevelManifest.cs
+++ Common/Scripts/DataModel/Static/LevelManifest.cs
@@ -5,6 +5,16 @@ namespace FastDragon
     [GlobalClass]
     public partial class LevelManifest : Resource
     {
+        /// <summary>
+        ///     Indicates that this level is a debug level.
+        ///     Debug levels are not meant to be accessible in the published
+        ///     game.
+        ///
+        ///     Debug levels should not be included in any UI elements that
+        ///     list all level in the game.
+        /// </summary>
+        [Export] public bool Debug { get; set; }
+
         [Export] public string HumanReadableName { get; set; }
         [Export(PropertyHint.FilePath)] public string SkyBoxEnvironmentFilePath { get; set; }
             = "res://Common/Skyboxes/DigitalWorld/DigitalWorldSkybox.tres";
