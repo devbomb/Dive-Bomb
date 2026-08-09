43c76496f8152ef78b56674308ef27c690fff15a
Alex Shelley <alexthepeppershelley@gmail.com>
2026-08-09 15:00:42 -0400

    Change the environment to a file path

--- Common/Scripts/DataModel/Static/LevelManifest.cs
+++ Common/Scripts/DataModel/Static/LevelManifest.cs
@@ -5,11 +5,10 @@ namespace FastDragon
     [GlobalClass]
     public partial class LevelManifest : Resource
     {
-        private const string DefaultSkyboxPath = "res://Common/Skyboxes/DigitalWorld/DigitalWorldSkybox.tres";
         [Export] public string LevelId { get; set; }
         [Export] public string HumanReadableName { get; set; }
-        [Export] public Environment SkyBoxEnvironment { get; set; }
-            = ResourceLoader.Load<Environment>(DefaultSkyboxPath);
+        [Export(PropertyHint.FilePath)] public string SkyBoxEnvironmentFilePath { get; set; }
+            = "res://Common/Skyboxes/DigitalWorld/DigitalWorldSkybox.tres";
         [Export(PropertyHint.FilePath)] public string SceneFilePath { get; set; }
     }
 }
