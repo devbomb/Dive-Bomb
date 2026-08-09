a6ad17cfe0395aae78300ad76bb5ab5e0dfa6a87
Alex Shelley <alexthepeppershelley@gmail.com>
2026-08-09 15:57:38 -0400

    Remove LevelId; we'll be using the resource path instead

--- Common/Scripts/DataModel/Static/LevelManifest.cs
+++ Common/Scripts/DataModel/Static/LevelManifest.cs
@@ -5,7 +5,6 @@ namespace FastDragon
     [GlobalClass]
     public partial class LevelManifest : Resource
     {
-        [Export] public string LevelId { get; set; }
         [Export] public string HumanReadableName { get; set; }
         [Export(PropertyHint.FilePath)] public string SkyBoxEnvironmentFilePath { get; set; }
             = "res://Common/Skyboxes/DigitalWorld/DigitalWorldSkybox.tres";
