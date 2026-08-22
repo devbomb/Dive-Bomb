4cfbf5cb1ad039453ab51ba36537ea56b9cb2f8d
Alex Shelley <alexthepeppershelley@gmail.com>
2026-08-22 15:58:53 -0400

    Fix the escape sequence door instantly opening when the cutscene finishes, instead of slowly opening.

--- Entities/Wall/OpenableWall/OpenableWall.cs
+++ Entities/Wall/OpenableWall/OpenableWall.cs
@@ -13,8 +13,6 @@ namespace FastDragon
         [Export] public double OpenDuration = 0.5;
         [Export] public double CloseDuration = 0.5;
 
-        public bool IsMoving => _stateMachine.CurrentState is Closing or Opening;
-
         private bool _initialized;
         private Vector3 _closedPos;
         private Vector3 _openPos;
